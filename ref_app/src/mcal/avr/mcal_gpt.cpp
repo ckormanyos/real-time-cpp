25cd663676c0df130353d2d0c8de0d6665bd7c6f
Christopher Kormanyos <e_float@yahoo.com>
2013-05-05 10:20:24 +0200

    Changed AVR(R) time base to timer0 overflow. Repaired (again) the AtmelStudio(R) solution project file.

--- ref_app/src/mcal/avr/mcal_gpt.cpp
+++ ref_app/src/mcal/avr/mcal_gpt.cpp
@@ -44,40 +44,42 @@ namespace
   }
 }
 
-extern "C" void __vector_14() __attribute__((signal, used, externally_visible));
+extern "C" void __vector_16() __attribute__((signal, used, externally_visible));
 
-void __vector_14()
+void __vector_16(void)
 {
-  // This interrupt occurs every 128us.
-  // Increment the 32-bit system tick by 128.
-  system_tick += std::uint8_t(0x80U);
-}
-
-void mcal::gpt::init(const config_type*)
-{
-  // Clear the timer0 overflow flag.
-  mcal::reg::access<std::uint8_t, std::uint8_t, mcal::reg::tifr0, 0x02U>::reg_set();
-
-  // Enable the compare match a interrupt.
-  mcal::reg::access<std::uint8_t, std::uint8_t, mcal::reg::timsk0, 0x02U>::reg_set();
-
-  // Set ctc mode 2 for counter timer0 compare.
-  mcal::reg::access<std::uint8_t, std::uint8_t, mcal::reg::tccr0a, 0x02U>::reg_set();
-
-  // Set the compare match a value to 255.
-  mcal::reg::access<std::uint8_t, std::uint8_t, mcal::reg::ocr0a, 0xFFU>::reg_set();
-
-  // Set the timer0 clock source to f_osc/8 = 2MHz and begin counting.
-  mcal::reg::access<std::uint8_t, std::uint8_t, mcal::reg::tccr0b, 0x02U>::reg_set();
-
-  // Set the is-initialized indication flag.
-  gpt_is_initialized() = true;
+  // Increment the software system tick.
+  system_tick += static_cast<std::uint8_t>(0x80U);
 }
 
 mcal::gpt::value_type mcal::gpt::get_time_elapsed()
 {
   return (gpt_is_initialized() ? consistent_microsecond_tick()
                                : mcal::gpt::value_type(0U));
+}
+
+void mcal::gpt::init(const config_type*)
+{
+  // Clear the timer0 overflow flag.
+  mcal::reg::access<std::uint8_t,
+                    std::uint8_t,
+                    mcal::reg::tifr0,
+                    0x01U>::reg_set();
+
+  // Enable the timer0 overflow interrupt.
+  mcal::reg::access<std::uint8_t,
+                    std::uint8_t,
+                    mcal::reg::timsk0,
+                    0x01U>::reg_set();
+
+  // Set the timer0 clock source to f_osc/8 = 2MHz and begin counting.
+  mcal::reg::access<std::uint8_t,
+                    std::uint8_t,
+                    mcal::reg::tccr0b,
+                    0x02U>::reg_set();
+
+  // Set the is-initialized indication flag.
+  gpt_is_initialized() = true;
 }
 
 // Implement std::chrono::high_resolution_clock::now()
--- ref_app/target/micros/avr/startup/int_vect.cpp
+++ ref_app/target/micros/avr/startup/int_vect.cpp
@@ -10,7 +10,7 @@
 
 extern "C" void startup();
 extern "C" void __vector_unused_irq() __attribute__((signal, used, externally_visible));
-extern "C" void __vector_14        () __attribute__((signal, used, externally_visible));
+extern "C" void __vector_16        () __attribute__((signal, used, externally_visible));
 extern "C" void __vector_17        () __attribute__((signal, used, externally_visible));
 
 extern "C" void __vector_unused_irq() { for(;;) { mcal::cpu::nop(); } }
@@ -31,30 +31,30 @@ extern "C"
 const volatile isr_type isr_vector[26U] =
 {
                                             // Vector No.  Program Address  Source Interrupt Definition
-  {{0x0C, 0x94}, startup },                 //  1 reset
-  {{0x0C, 0x94}, __vector_unused_irq},      //  2 ext0
-  {{0x0C, 0x94}, __vector_unused_irq},      //  3 ext1
-  {{0x0C, 0x94}, __vector_unused_irq},      //  4 pin0
-  {{0x0C, 0x94}, __vector_unused_irq},      //  5 pin1
-  {{0x0C, 0x94}, __vector_unused_irq},      //  6 pin2
-  {{0x0C, 0x94}, __vector_unused_irq},      //  7 watchdog
-  {{0x0C, 0x94}, __vector_unused_irq},      //  8 timer2 cmp a
-  {{0x0C, 0x94}, __vector_unused_irq},      //  9 timer2 cmp b
-  {{0x0C, 0x94}, __vector_unused_irq},      // 10 timer2 ovf
-  {{0x0C, 0x94}, __vector_unused_irq},      // 11 timer1 cap
-  {{0x0C, 0x94}, __vector_unused_irq},      // 12 timer1 cmp a
-  {{0x0C, 0x94}, __vector_unused_irq},      // 13 timer1 cmp b
-  {{0x0C, 0x94}, __vector_unused_irq},      // 14 timer1 ovf
-  {{0x0C, 0x94}, __vector_14},              // 15 timer0 cmp a
-  {{0x0C, 0x94}, __vector_unused_irq},      // 16 timer0 cmp b
-  {{0x0C, 0x94}, __vector_unused_irq},      // 17 timer0 ovf
-  {{0x0C, 0x94}, __vector_17},              // 18 spi(TM)
-  {{0x0C, 0x94}, __vector_unused_irq},      // 19 usart rx
-  {{0x0C, 0x94}, __vector_unused_irq},      // 20 usart err
-  {{0x0C, 0x94}, __vector_unused_irq},      // 21 usart rx
-  {{0x0C, 0x94}, __vector_unused_irq},      // 22 adc
-  {{0x0C, 0x94}, __vector_unused_irq},      // 23 eep Ready
-  {{0x0C, 0x94}, __vector_unused_irq},      // 24 comparator
-  {{0x0C, 0x94}, __vector_unused_irq},      // 25 two-wire
-  {{0x0C, 0x94}, __vector_unused_irq}       // 26 spm
+  {{0x0C, 0x94}, startup },                 //  0 reset
+  {{0x0C, 0x94}, __vector_unused_irq},      //  1 ext0
+  {{0x0C, 0x94}, __vector_unused_irq},      //  2 ext1
+  {{0x0C, 0x94}, __vector_unused_irq},      //  3 pin0
+  {{0x0C, 0x94}, __vector_unused_irq},      //  4 pin1
+  {{0x0C, 0x94}, __vector_unused_irq},      //  5 pin2
+  {{0x0C, 0x94}, __vector_unused_irq},      //  6 watchdog
+  {{0x0C, 0x94}, __vector_unused_irq},      //  7 timer2 cmp a
+  {{0x0C, 0x94}, __vector_unused_irq},      //  8 timer2 cmp b
+  {{0x0C, 0x94}, __vector_unused_irq},      //  9 timer2 ovf
+  {{0x0C, 0x94}, __vector_unused_irq},      // 10 timer1 cap
+  {{0x0C, 0x94}, __vector_unused_irq},      // 11 timer1 cmp a
+  {{0x0C, 0x94}, __vector_unused_irq},      // 12 timer1 cmp b
+  {{0x0C, 0x94}, __vector_unused_irq},      // 13 timer1 ovf
+  {{0x0C, 0x94}, __vector_unused_irq},      // 14 timer0 cmp a
+  {{0x0C, 0x94}, __vector_unused_irq},      // 15 timer0 cmp b
+  {{0x0C, 0x94}, __vector_16},              // 16 timer0 ovf
+  {{0x0C, 0x94}, __vector_17},              // 17 spi(TM)
+  {{0x0C, 0x94}, __vector_unused_irq},      // 18 usart rx
+  {{0x0C, 0x94}, __vector_unused_irq},      // 19 usart err
+  {{0x0C, 0x94}, __vector_unused_irq},      // 20 usart rx
+  {{0x0C, 0x94}, __vector_unused_irq},      // 21 adc
+  {{0x0C, 0x94}, __vector_unused_irq},      // 22 eep Ready
+  {{0x0C, 0x94}, __vector_unused_irq},      // 23 comparator
+  {{0x0C, 0x94}, __vector_unused_irq},      // 24 two-wire
+  {{0x0C, 0x94}, __vector_unused_irq}       // 25 spm
 };
