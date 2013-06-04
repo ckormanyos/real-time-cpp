985a79ec9ba4aeb7df16ca616e0666f91d958e07
Christopher Kormanyos <e_float@yahoo.com>
2013-06-05 00:19:31 +0200

    Optimize the single-pin debug monitor.

--- ref_app/src/util/monitor/util_single_pin_debug_monitor.h
+++ ref_app/src/util/monitor/util_single_pin_debug_monitor.h
@@ -1,3 +1,10 @@
+///////////////////////////////////////////////////////////////////////////////
+//  Copyright Christopher Kormanyos 2013.
+//  Distributed under the Boost Software License,
+//  Version 1.0. (See accompanying file LICENSE_1_0.txt
+//  or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
 #ifndef _UTIL_SINGLE_PIN_DEBUG_MONITOR_2013_05_16_H_
   #define _UTIL_SINGLE_PIN_DEBUG_MONITOR_2013_05_16_H_
 
@@ -15,17 +22,26 @@
       void driver_task();
 
     private:
-      static const std::uint_least16_t driver_delay_recv_init_microseconds = std::uint_least16_t(1000U - 50U);
-      static const std::uint_least16_t driver_delay_recv_stat_microseconds = std::uint_least16_t(std::uint_least16_t(driver_delay_milliseconds) * 1000U) - 50U;
+      // Set up the port driver timing.
+      // Here, we are implementing a software UART signal with 250 bits-per-second.
+      // This corresponds to a time of approximately four milliseconds per bit.
+      // The polling period is one millisecond, such that the
+      // driver polls up to four times per bit.
+
+      // * The single-bit timeout is set to 5% less than four poll times.
+      // * The receiver automatically resets if it is idle for 12 bit times or more.
+      static const std::uint_least16_t bit_time_microseconds = 950U * 4U;
+      static const std::uint_least8_t  receive_reset_limit   =  12U * 4U;
 
       typedef util::timer<std::uint16_t> timer_type;
 
-      timer_type          driver_wait;
-      std::uint_least8_t  driver_current_bit_position;
+      timer_type         driver_wait;
+      std::uint_least8_t driver_current_bit_position;
 
-      void driver_init_port() const;
-      void port_pin_send(const bool value_is_high) const;
-      bool port_pin_receive() const;
+      static void driver_init_port(const bool set_direction_to_output);
+      static void port_pin_send(const bool set_value_to_high);
+      static bool port_pin_receive();
+
       void driver_port_send();
       void driver_port_receive();
     };
@@ -46,12 +62,12 @@
       }
       else
       {
-        // clear buffer automatically if timeout detected
+        // Clear the buffer if nothing has been received for a while.
         if(port_pin_receive())
         {
           ++driver_received_nothing_counter;
 
-          if(driver_received_nothing_counter > driver_receive_reset)
+          if(driver_received_nothing_counter > receive_reset_limit)
           {
             driver_flush_buffer();
             driver_received_nothing_counter = 0U;
@@ -67,20 +83,20 @@
     }
 
     template<typename port_pin_type>
-    void single_pin_debug_monitor<port_pin_type>::driver_init_port() const
+    void single_pin_debug_monitor<port_pin_type>::driver_init_port(const bool set_direction_to_output)
     {
-      (driver_is_in_send_mode ? port_pin_type::set_direction_output()
-                              : port_pin_type::set_direction_input());
+      (set_direction_to_output ? port_pin_type::set_direction_output()
+                               : port_pin_type::set_direction_input());
     }
 
     template<typename port_pin_type>
-    void single_pin_debug_monitor<port_pin_type>::port_pin_send(const bool value_is_high) const
+    void single_pin_debug_monitor<port_pin_type>::port_pin_send(const bool set_value_to_high)
     {
-      (value_is_high ? port_pin_type::set_pin_high() : port_pin_type::set_pin_low());
+      (set_value_to_high ? port_pin_type::set_pin_high() : port_pin_type::set_pin_low());
     }
 
     template<typename port_pin_type>
-    bool single_pin_debug_monitor<port_pin_type>::port_pin_receive() const
+    bool single_pin_debug_monitor<port_pin_type>::port_pin_receive()
     {
       return port_pin_type::read_input_value();
     }
@@ -90,34 +106,35 @@
     {
       // Send the buffer contents over the port pin.
       // Use the UART physical layer protocol.
-      if(driver_current_byte_value_or_position < driver_byte_buffer_length)
+      if(driver_current_byte_value_or_position < driver_buffer_length)
       {
         switch(driver_transmit_state)
         {
           case send_start_bit:
+            driver_wait.start_relative(timer_type::microseconds(bit_time_microseconds));
+
             if(driver_current_byte_value_or_position == 0U)
             {
-              driver_init_port();
+              driver_init_port(true);
             }
 
             port_pin_send(false);
-            driver_wait.start_relative(timer_type::microseconds(driver_delay_recv_stat_microseconds));
             driver_transmit_state = send_data_bits;
             driver_current_bit_position = 0U;
 
           case send_data_bits:
             if(driver_wait.timeout())
             {
-              driver_wait.start_relative(timer_type::microseconds(driver_delay_recv_stat_microseconds));
+              driver_wait.start_relative(timer_type::microseconds(bit_time_microseconds));
 
-              // Sent a single data bit.
-              const bool bit_is_high = static_cast<std::uint_fast8_t>(driver_byte_buffer[driver_current_byte_value_or_position] & std::uint8_t(1U << driver_current_bit_position)) != static_cast<std::uint_fast8_t>(0U);
+              // Send a single data bit.
+              const bool bit_is_high = static_cast<std::uint_fast8_t>(driver_buffer[driver_current_byte_value_or_position] & std::uint8_t(1U << driver_current_bit_position)) != static_cast<std::uint_fast8_t>(0U);
 
               port_pin_send(bit_is_high);
 
               ++driver_current_bit_position;
 
-              if(driver_current_bit_position >= driver_byte_buffer.size())
+              if(driver_current_bit_position >= driver_buffer.size())
               {
                 driver_transmit_state = send_stop_bit;
               }
@@ -127,9 +144,9 @@
           case send_stop_bit:
             if(driver_wait.timeout())
             {
+              driver_wait.start_relative(timer_type::microseconds(bit_time_microseconds));
               port_pin_send(true);
               driver_transmit_state = send_pause_for_next_byte;
-              driver_wait.start_relative(timer_type::microseconds(driver_delay_recv_stat_microseconds));
             }
             break;
 
@@ -140,11 +157,11 @@
 
               driver_transmit_state = send_start_bit;
 
-              if(driver_current_byte_value_or_position == driver_byte_buffer_length)
+              if(driver_current_byte_value_or_position >= driver_buffer_length)
               {
                 driver_is_in_send_mode = false;
                 driver_flush_buffer();
-                driver_init_port();
+                driver_init_port(driver_is_in_send_mode);
               }
             }
             break;
@@ -160,46 +177,42 @@
     {
       // Read incoming data bytes on the port pin and store them in the buffer.
       // Use the UART physical layer protocol.
-      if(driver_byte_buffer_length < driver_byte_buffer.size())
+      if(driver_buffer_length < driver_buffer.size())
       {
         switch(driver_transmit_state)
         {
           case recieve_start_bit:
             if((!port_pin_receive()))
             {
-              driver_wait.start_relative(timer_type::microseconds(driver_delay_recv_init_microseconds));
               driver_transmit_state = recieve_start_bit_validation;
             }
             break;
 
           case recieve_start_bit_validation:
-            if(driver_wait.timeout())
+            if((!port_pin_receive()))
             {
-              if((!port_pin_receive()))
-              {
-                driver_wait.start_relative(timer_type::microseconds(driver_delay_recv_stat_microseconds));
-                driver_current_bit_position           = 0U;
-                driver_current_byte_value_or_position = 0U;
-                driver_transmit_state                 = recieve_data_bits;
-              }
-              else
-              {
-                // If the start bit can not be validated, then
-                // flush the buffer and revert to the receive state.
-                driver_flush_buffer();
-              }
+              driver_wait.start_relative(timer_type::microseconds(bit_time_microseconds));
+              driver_current_bit_position           = 0U;
+              driver_current_byte_value_or_position = 0U;
+              driver_transmit_state                 = recieve_data_bits;
+            }
+            else
+            {
+              // If the start bit can not be validated, then flush
+              // the buffer and thereby revert to the receive state.
+              driver_flush_buffer();
             }
             break;
 
           case recieve_data_bits:
             if(driver_wait.timeout())
             {
-              driver_wait.start_relative(timer_type::microseconds(driver_delay_recv_stat_microseconds));
+              driver_wait.start_relative(timer_type::microseconds(bit_time_microseconds));
 
               // Append the bit value to the current byte value.
               if(port_pin_receive())
               {
-                driver_current_byte_value_or_position |= static_cast<std::uint8_t>(1U << driver_current_bit_position);
+                driver_current_byte_value_or_position |= static_cast<std::uint8_t>(std::uint_least8_t(1U) << driver_current_bit_position);
               }
 
               ++driver_current_bit_position;
@@ -216,9 +229,9 @@
             {
               if(port_pin_receive())
               {
-                driver_byte_buffer[driver_byte_buffer_length] = driver_current_byte_value_or_position;
+                driver_buffer[driver_buffer_length] = driver_current_byte_value_or_position;
 
-                ++driver_byte_buffer_length;
+                ++driver_buffer_length;
               }
 
               driver_transmit_state = recieve_pause_for_next_byte;
--- ref_app/src/util/monitor/util_single_pin_debug_monitor_base.h
+++ ref_app/src/util/monitor/util_single_pin_debug_monitor_base.h
@@ -1,3 +1,10 @@
+///////////////////////////////////////////////////////////////////////////////
+//  Copyright Christopher Kormanyos 2013.
+//  Distributed under the Boost Software License,
+//  Version 1.0. (See accompanying file LICENSE_1_0.txt
+//  or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
 #ifndef _UTIL_SINGLE_PIN_DEBUG_MONITOR_BASE_2013_05_16_H_
   #define _UTIL_SINGLE_PIN_DEBUG_MONITOR_BASE_2013_05_16_H_
 
@@ -19,11 +26,9 @@
       void protocol_task();
 
     protected:
-      static const std::uint_least8_t driver_buffer_length      = 8U;
-      static const std::uint_least8_t driver_delay_milliseconds = 4U;
-      static const std::uint_least8_t driver_receive_reset      = driver_delay_milliseconds * 12U;
-
-      typedef std::array<std::uint8_t, driver_buffer_length> driver_byte_buffer_type;
+      // Set up the driver buffer.
+      static const std::uint_least8_t driver_buffer_size = 8U;
+      typedef std::array<std::uint8_t, driver_buffer_size> driver_buffer_type;
 
       typedef enum enum_driver_transmit_state_type
       {
@@ -39,15 +44,15 @@
       }
       driver_transmit_state_type;
 
-      driver_byte_buffer_type     driver_byte_buffer;
-      std::uint_least8_t          driver_byte_buffer_length;
+      driver_buffer_type          driver_buffer;
+      std::uint_least8_t          driver_buffer_length;
       bool                        driver_is_in_send_mode;
       std::uint_least8_t          driver_current_byte_value_or_position;
       std::uint_least8_t          driver_received_nothing_counter;
       driver_transmit_state_type  driver_transmit_state;
 
-      single_pin_debug_monitor_base() : driver_byte_buffer(),
-                                        driver_byte_buffer_length(0U),
+      single_pin_debug_monitor_base() : driver_buffer(),
+                                        driver_buffer_length(0U),
                                         driver_is_in_send_mode(false),
                                         driver_current_byte_value_or_position(0U),
                                         driver_received_nothing_counter(0U),
@@ -65,28 +70,41 @@
 
       void driver_flush_buffer()
       {
-        driver_byte_buffer_length = 0U;
-        driver_transmit_state     = recieve_start_bit;
-        std::fill(driver_byte_buffer.begin(), driver_byte_buffer.end(), driver_byte_buffer_type::value_type(0U));
+        driver_buffer_length  = 0U;
+        driver_transmit_state = recieve_start_bit;
+        std::fill(driver_buffer.begin(), driver_buffer.end(), driver_buffer_type::value_type(0U));
       }
     };
 
     template<typename addr_type, const addr_type addr_offset>
     void single_pin_debug_monitor_base::protocol_task()
     {
+      // This protocol task embodies the "BWD" protocol.
+      // 'b' means read a byte.
+      // 'w' means read a word.
+      // 'd' means read a dword.
+      // 'B' means write a byte.
+      // 'W' means write a word.
+      // 'D' means write a dword.
+
       if(driver_is_in_send_mode)
       {
         return;
       }
 
-      // Obtain the service ID for a read or write command,
-      // and set the number of data elements in the command.
+      // Note: These variables are shielded from aggressive optimization
+      // with the volatile qualifier. This is because direct memory access can,
+      // for a highly optimizing compiler, *seem* to simply do nothing.
+      // The volatile qualification reduces the risk of the memory access
+      // code being optimized away entirely.
 
-      // Note: These variables are shielded from aggressive optimization.
       volatile std::uint_least8_t data_elements;
       volatile std::uint_least8_t service_id;
 
-      switch(driver_byte_buffer[0U])
+      // Obtain the service ID for a read or write command,
+      // and set the number of data elements in the command.
+
+      switch(driver_buffer[0U])
       {
         default:
         case 0x62:  // 'b'
@@ -124,13 +142,13 @@
       // The width of this address might exceed 16 bits.
       const addr_type address = 
          addr_offset
-       + addr_type(util::make_long<std::uint8_t, std::uint16_t>(driver_byte_buffer[1U],
-                                                                driver_byte_buffer[2U]));
+       + addr_type(util::make_long<std::uint8_t, std::uint16_t>(driver_buffer[1U],
+                                                                driver_buffer[2U]));
 
       // Evaluate the service ID and issue the read or write command.
       if(service_id == 1U)
       {
-        if(driver_byte_buffer_length == 3U)
+        if(driver_buffer_length == 3U)
         {
           switch(data_elements)
           {
@@ -138,7 +156,7 @@
             default:
               {
                 // Read a byte with the command 'b'.
-                driver_byte_buffer[0U] = *reinterpret_cast<volatile std::uint8_t*>(address);
+                driver_buffer[0U] = *reinterpret_cast<volatile std::uint8_t*>(address);
               }
               break;
 
@@ -147,8 +165,8 @@
                 // Read a word with the command 'w'.
                 const std::uint16_t value = *reinterpret_cast<volatile std::uint16_t*>(address);
 
-                driver_byte_buffer[1U] = util::hi_part<std::uint8_t, std::uint16_t>(value);
-                driver_byte_buffer[0U] = util::lo_part<std::uint8_t, std::uint16_t>(value);
+                driver_buffer[1U] = util::hi_part<std::uint8_t, std::uint16_t>(value);
+                driver_buffer[0U] = util::lo_part<std::uint8_t, std::uint16_t>(value);
               }
               break;
 
@@ -160,22 +178,22 @@
                 const std::uint16_t value_lo = util::lo_part<std::uint16_t, std::uint32_t>(value);
                 const std::uint16_t value_hi = util::hi_part<std::uint16_t, std::uint32_t>(value);
 
-                driver_byte_buffer[3U] = util::hi_part<std::uint8_t, std::uint16_t>(value_hi);
-                driver_byte_buffer[2U] = util::lo_part<std::uint8_t, std::uint16_t>(value_hi);
-                driver_byte_buffer[1U] = util::hi_part<std::uint8_t, std::uint16_t>(value_lo);
-                driver_byte_buffer[0U] = util::lo_part<std::uint8_t, std::uint16_t>(value_lo);
+                driver_buffer[3U] = util::hi_part<std::uint8_t, std::uint16_t>(value_hi);
+                driver_buffer[2U] = util::lo_part<std::uint8_t, std::uint16_t>(value_hi);
+                driver_buffer[1U] = util::hi_part<std::uint8_t, std::uint16_t>(value_lo);
+                driver_buffer[0U] = util::lo_part<std::uint8_t, std::uint16_t>(value_lo);
               }
               break;
           }
 
-          driver_byte_buffer_length = data_elements;
+          driver_buffer_length = data_elements;
 
           driver_send_buffer();
         }
       }
       else if(service_id == 2U)
       {
-        if(driver_byte_buffer_length == (data_elements + 3U))
+        if(driver_buffer_length == (data_elements + 3U))
         {
           switch(data_elements)
           {
@@ -183,7 +201,7 @@
             default:
               {
                 // Write a byte with the command 'B'.
-                *reinterpret_cast<std::uint8_t*>(address) = driver_byte_buffer[3U];
+                *reinterpret_cast<std::uint8_t*>(address) = driver_buffer[3U];
               }
               break;
 
@@ -191,8 +209,8 @@
               {
                 // Write a word with the command 'W'.
                 const std::uint16_t value =
-                  util::make_long<std::uint8_t, std::uint16_t>(static_cast<std::uint8_t>(driver_byte_buffer[3U]),
-                                                                static_cast<std::uint8_t>(driver_byte_buffer[4U]));
+                  util::make_long<std::uint8_t, std::uint16_t>(static_cast<std::uint8_t>(driver_buffer[3U]),
+                                                               static_cast<std::uint8_t>(driver_buffer[4U]));
 
                 *reinterpret_cast<volatile std::uint16_t*>(address) = value;
               }
@@ -202,12 +220,12 @@
               {
                 // Write a dword with the command 'D'.
                 const std::uint16_t value_lo =
-                  util::make_long<std::uint8_t, std::uint16_t>(static_cast<std::uint8_t>(driver_byte_buffer[3U]),
-                                                                static_cast<std::uint8_t>(driver_byte_buffer[4U]));
+                  util::make_long<std::uint8_t, std::uint16_t>(static_cast<std::uint8_t>(driver_buffer[3U]),
+                                                               static_cast<std::uint8_t>(driver_buffer[4U]));
 
                 const std::uint16_t value_hi =
-                  util::make_long<std::uint8_t, std::uint16_t>(static_cast<std::uint8_t>(driver_byte_buffer[5U]),
-                                                                static_cast<std::uint8_t>(driver_byte_buffer[6U]));
+                  util::make_long<std::uint8_t, std::uint16_t>(static_cast<std::uint8_t>(driver_buffer[5U]),
+                                                               static_cast<std::uint8_t>(driver_buffer[6U]));
 
                 *reinterpret_cast<volatile std::uint32_t*>(address) = util::make_long<std::uint16_t, std::uint32_t>(value_lo, value_hi);
               }
