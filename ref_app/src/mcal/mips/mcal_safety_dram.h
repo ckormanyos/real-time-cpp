cd6e1b98ed730f0b49a2d78d37a591fd8e520d4d
Christopher Kormanyos <e_float@yahoo.com>
2013-12-11 22:00:21 +0100

    Add licensing information.

--- ref_app/src/mcal/mips/mcal_safety_dram.h
+++ ref_app/src/mcal/mips/mcal_safety_dram.h
@@ -1,3 +1,10 @@
+///////////////////////////////////////////////////////////////////////////////
+//  Copyright Christopher Kormanyos 2013.
+//  Distributed under the Boost Software License,
+//  Version 1.0. (See accompanying file LICENSE_1_0.txt
+//  or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
 #ifndef _MCAL_SAFETY_DRAM_2013_11_25_H_
   #define _MCAL_SAFETY_DRAM_2013_11_25_H_
 
