650c662b3a78a33d939c635443383b44201ab70a
ckormanyos <e_float@yahoo.com>
2014-04-27 22:29:53 +0200

    Extend the FPU benchmark cases.

--- ref_app/src/app/benchmark/app_benchmark_fpu.cpp
+++ ref_app/src/app/benchmark/app_benchmark_fpu.cpp
@@ -6,7 +6,7 @@
 //  or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
-//#define CFG_USE_APP_BENCHMARK_FPU
+#define CFG_USE_APP_BENCHMARK_FPU
 
 #if defined(CFG_USE_APP_BENCHMARK_FPU)
 
@@ -16,21 +16,26 @@
   #include <limits>
   #include <mcal_benchmark.h>
 
-  #define CFG_APP_BENCHMARK_FPU_TYPE_SQRT     1
-  #define CFG_APP_BENCHMARK_FPU_TYPE_SIN      2
-  #define CFG_APP_BENCHMARK_FPU_TYPE_COS      3
-  #define CFG_APP_BENCHMARK_FPU_TYPE_TAN      4
-  #define CFG_APP_BENCHMARK_FPU_TYPE_ASIN     5
-  #define CFG_APP_BENCHMARK_FPU_TYPE_ACOS     6
-  #define CFG_APP_BENCHMARK_FPU_TYPE_ATAN     7
-  #define CFG_APP_BENCHMARK_FPU_TYPE_LOG      8
-  #define CFG_APP_BENCHMARK_FPU_TYPE_EXP      9
-  #define CFG_APP_BENCHMARK_FPU_TYPE_POW     10
-  #define CFG_APP_BENCHMARK_FPU_TYPE_SINH    11
-  #define CFG_APP_BENCHMARK_FPU_TYPE_COSH    12
-  #define CFG_APP_BENCHMARK_FPU_TYPE_TANH    13
-  #define CFG_APP_BENCHMARK_FPU_TYPE_GAMMA   14
+  #define CFG_APP_BENCHMARK_FPU_TYPE_FLOOR    1
+  #define CFG_APP_BENCHMARK_FPU_TYPE_CEIL     2
+  #define CFG_APP_BENCHMARK_FPU_TYPE_SQRT     3
+  #define CFG_APP_BENCHMARK_FPU_TYPE_SIN      4
+  #define CFG_APP_BENCHMARK_FPU_TYPE_COS      5
+  #define CFG_APP_BENCHMARK_FPU_TYPE_TAN      6
+  #define CFG_APP_BENCHMARK_FPU_TYPE_ASIN     7
+  #define CFG_APP_BENCHMARK_FPU_TYPE_ACOS     8
+  #define CFG_APP_BENCHMARK_FPU_TYPE_ATAN     9
+  #define CFG_APP_BENCHMARK_FPU_TYPE_EXP     10
+  #define CFG_APP_BENCHMARK_FPU_TYPE_POW     11
+  #define CFG_APP_BENCHMARK_FPU_TYPE_LOG     12
+  #define CFG_APP_BENCHMARK_FPU_TYPE_LOG10   13
+  #define CFG_APP_BENCHMARK_FPU_TYPE_SINH    14
+  #define CFG_APP_BENCHMARK_FPU_TYPE_COSH    15
+  #define CFG_APP_BENCHMARK_FPU_TYPE_TANH    16
+  #define CFG_APP_BENCHMARK_FPU_TYPE_GAMMA   17
 
+//  #define CFG_APP_BENCHMARK_FPU_TYPE CFG_APP_BENCHMARK_FPU_TYPE_FLOOR
+//  #define CFG_APP_BENCHMARK_FPU_TYPE CFG_APP_BENCHMARK_FPU_TYPE_CEIL
   #define CFG_APP_BENCHMARK_FPU_TYPE CFG_APP_BENCHMARK_FPU_TYPE_SQRT
 //  #define CFG_APP_BENCHMARK_FPU_TYPE CFG_APP_BENCHMARK_FPU_TYPE_SIN
 //  #define CFG_APP_BENCHMARK_FPU_TYPE CFG_APP_BENCHMARK_FPU_TYPE_COS
@@ -38,9 +43,10 @@
 //  #define CFG_APP_BENCHMARK_FPU_TYPE CFG_APP_BENCHMARK_FPU_TYPE_ASIN
 //  #define CFG_APP_BENCHMARK_FPU_TYPE CFG_APP_BENCHMARK_FPU_TYPE_ACOS
 //  #define CFG_APP_BENCHMARK_FPU_TYPE CFG_APP_BENCHMARK_FPU_TYPE_ATAN
-//  #define CFG_APP_BENCHMARK_FPU_TYPE CFG_APP_BENCHMARK_FPU_TYPE_LOG
 //  #define CFG_APP_BENCHMARK_FPU_TYPE CFG_APP_BENCHMARK_FPU_TYPE_EXP
 //  #define CFG_APP_BENCHMARK_FPU_TYPE CFG_APP_BENCHMARK_FPU_TYPE_POW
+//  #define CFG_APP_BENCHMARK_FPU_TYPE CFG_APP_BENCHMARK_FPU_TYPE_LOG
+//  #define CFG_APP_BENCHMARK_FPU_TYPE CFG_APP_BENCHMARK_FPU_TYPE_LOG10
 //  #define CFG_APP_BENCHMARK_FPU_TYPE CFG_APP_BENCHMARK_FPU_TYPE_SINH
 //  #define CFG_APP_BENCHMARK_FPU_TYPE CFG_APP_BENCHMARK_FPU_TYPE_COSH
 //  #define CFG_APP_BENCHMARK_FPU_TYPE CFG_APP_BENCHMARK_FPU_TYPE_TANH
@@ -55,6 +61,18 @@
       const std::float32_t delta = std::fabs(FLOAT32_C(1.0) - ratio);
 
       return (delta < tol);
+    }
+
+    template<typename float_type>
+    const float_type root_two()
+    {
+      return static_cast<float_type>(FLOATMAX_C(1.4142135623730950488016887242096980785697));
+    }
+
+    template<typename float_type>
+    const float_type pi()
+    {
+      return static_cast<float_type>(FLOATMAX_C(3.1415926535897932384626433832795028841972));
     }
 
     std::float32_t value_x;
@@ -94,7 +112,22 @@ void app::benchmark::fpu::task_func()
 
   mcal::benchmark::benchmark_port_type::set_pin_high();
 
-  #if(CFG_APP_BENCHMARK_FPU_TYPE == CFG_APP_BENCHMARK_FPU_TYPE_SQRT)
+
+  #if(CFG_APP_BENCHMARK_FPU_TYPE == CFG_APP_BENCHMARK_FPU_TYPE_FLOOR)
+
+    // The expected value is: floor(pi) = (approx.) 3.0.
+    value_y = std::floor(pi<std::float32_t>());
+    const bool the_result_is_ok = is_close_fraction(FLOAT32_C(3.0), value_y, tolerance);
+    static_cast<void>(value_x);
+
+  #elif(CFG_APP_BENCHMARK_FPU_TYPE == CFG_APP_BENCHMARK_FPU_TYPE_CEIL)
+
+    // The expected value is: ceil(root_two) = (approx.) 2.0.
+    value_y = std::ceil(root_two<std::float32_t>());
+    const bool the_result_is_ok = is_close_fraction(FLOAT32_C(2.0), value_y, tolerance);
+    static_cast<void>(value_x);
+
+  #elif(CFG_APP_BENCHMARK_FPU_TYPE == CFG_APP_BENCHMARK_FPU_TYPE_SQRT)
 
     // The expected value is: sqrt(0.5) = (approx.) 0.707106781.
     value_y = std::sqrt(value_x);
@@ -124,7 +157,7 @@ void app::benchmark::fpu::task_func()
     value_y = std::asin(value_x);
     const bool the_result_is_ok = is_close_fraction(FLOAT32_C(0.523598776), value_y, tolerance);
 
-  #elif(CFG_APP_BENCHMARK_FPU_TYPE == CFG_APP_BENCHMARK_FPU_TYPE_COS)
+  #elif(CFG_APP_BENCHMARK_FPU_TYPE == CFG_APP_BENCHMARK_FPU_TYPE_ACOS)
 
     // The expected value is: acos(0.5) = (approx.) 1.047197551.
     value_y = std::acos(value_x);
