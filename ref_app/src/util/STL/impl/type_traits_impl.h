7c8d7773ab7b6a9b167a107a7c3f71f3e8ba9764
ckormanyos <e_float@yahoo.com>
2014-05-22 20:30:15 +0200

    Re-factor the template parameters for clarity.

--- ref_app/src/util/STL/impl/type_traits_impl.h
+++ ref_app/src/util/STL/impl/type_traits_impl.h
@@ -10,22 +10,22 @@
 
   namespace std
   {
-    template<bool, typename template_type = void>
+    template<bool, typename template_value_type = void>
     struct enable_if { };
 
-    template<typename template_type>
-    struct enable_if<true, template_type>
+    template<typename template_value_type>
+    struct enable_if<true, template_value_type>
     {
-      typedef template_type type;
+      typedef template_value_type type;
     };
 
-    template<typename template_type, template_type the_value>
+    template<typename template_value_type, template_value_type the_value>
     struct integral_constant
     {
-      static const template_type my_value = the_value;
+      static const template_value_type my_value = the_value;
 
-      typedef template_type value_type;
-      typedef integral_constant<template_type, the_value> type;
+      typedef template_value_type value_type;
+      typedef integral_constant<template_value_type, the_value> type;
 
       operator value_type() const
       {
@@ -33,14 +33,14 @@
       }
     };
 
-    typedef integral_constant<bool, true> true_type;
+    typedef integral_constant<bool, true>  true_type;
     typedef integral_constant<bool, false> false_type;
 
-    template<typename template_type1, typename template_type2>
+    template<typename template_value_type1, typename template_value_type2>
     struct is_same : false_type { };
 
-    template<typename template_type1>
-    struct is_same<template_type1, template_type1> : true_type { };
+    template<typename template_value_type1>
+    struct is_same<template_value_type1, template_value_type1> : true_type { };
   }
 
 #endif // _TYPE_TRAITS_IMPL_2013_09_02_H_
