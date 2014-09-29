ea196dc657874294e83075cd661899fb54a39f1e
ckormanyos <e_float@yahoo.com>
2014-09-29 21:02:14 +0200

    Add license information.

--- ref_app/src/util/utility/util_countof.h
+++ ref_app/src/util/utility/util_countof.h
@@ -1,3 +1,10 @@
+///////////////////////////////////////////////////////////////////////////////
+//  Copyright Christopher Kormanyos 2014.
+//  Distributed under the Boost Software License,
+//  Version 1.0. (See accompanying file LICENSE_1_0.txt
+//  or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
 #ifndef _UTIL_COUNTOF_2014_09_29_H_
   #define _UTIL_COUNTOF_2014_09_29_H_
 
