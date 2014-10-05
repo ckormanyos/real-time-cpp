eef8265ca051cd6b798abf9f9fb80312d5e5cb13
ckormanyos <e_float@yahoo.com>
2014-10-05 20:54:05 +0200

    Repair real-valued integral template (and remove complex-valued one).

--- ref_app/src/math/calculus/integral.h
+++ ref_app/src/math/calculus/integral.h
@@ -8,96 +8,57 @@
 #ifndef _INTEGRAL_2012_01_09_H_
   #define _INTEGRAL_2012_01_09_H_
 
-  #include <cstdint>
-  #include <complex>
+  #include <algorithm>
+  #include <limits>
 
   template<typename real_value_type,
-           typename real_function_type>
+           typename real_function_type,
+           typename integer_value_type = std::uint_fast8_t>
   real_value_type integral(const real_value_type& a,
                            const real_value_type& b,
                            const real_value_type& tol,
-                           real_function_type real_function)
+                           real_function_type real_function,
+                           const integer_value_type k_max = (std::numeric_limits<integer_value_type>::max)())
   {
-    std::uint_fast8_t n = UINT8_C(1);
+    using std::abs;
 
-    real_value_type h = (b - a) / 2;
-    real_value_type I = (real_function(a) + real_function(b)) * h;
+    integer_value_type n2 = integer_value_type(1);
 
-    for(std::uint_fast8_t k = UINT8_C(0); k < UINT8_C(8); ++k)
+    real_value_type step_size       = (b - a) / 2;
+    real_value_type integral_result = (real_function(a) + real_function(b)) * step_size;
+
+    for(integer_value_type k = integer_value_type(0); k < k_max; ++k)
     {
       real_value_type sum(0);
 
-      for(std::uint_fast8_t j = UINT8_C(1); j <= n; ++j)
+      for(std::uint_fast8_t j = integer_value_type(1); j <= n2; ++j)
       {
-        sum += function(real_value_type(a + (real_value_type((j * 2) - 1) * h)));
+        sum += real_function(real_value_type(a + (real_value_type((j * 2) - 1) * step_size)));
       }
 
-      const real_value_type I0 = I;
-      I = (I / real_value_type(2)) + (h * sum);
+      const real_value_type integral_result_tmp = integral_result;
 
-      const real_value_type ratio = std::abs(I0 / I);
-      const real_value_type delta = std::abs(ratio - real_value_type(1));
+      integral_result = (integral_result / real_value_type(2)) + (step_size * sum);
 
-      if((k > UINT8_C(1)) && (delta < tol))
-      {
-        break;
-      }
+      const real_value_type ratio = abs(integral_result_tmp / integral_result);
+      const real_value_type delta = abs(ratio - real_value_type(1));
 
-      n *= 2;
-
-      h /= 2;
-    }
-
-    return I;
-  }
-
-  template<typename real_value_type,
-           typename complex_function_type>
-  std::complex<real_value_type> integral_complex(const real_value_type& a,
-                                                 const real_value_type& b,
-                                                 const real_value_type& tol,
-                                                 complex_function_type complex_function)
-  {
-    typedef std::complex<real_value_type> complex_value_type;
-
-    std::uint_fast8_t n = UINT8_C(1);
-
-    real_value_type    h = (b - a) / 2;
-    complex_value_type I = (complex_function(a) + complex_function(b)) * h;
-
-    for(std::uint_fast8_t k = UINT8_C(0); k < UINT8_C(8); ++k)
-    {
-      std::complex<real_value_type> sum(0);
-
-      for(std::uint_fast8_t j = UINT8_C(1); j <= n; ++j)
-      {
-        sum += complex_function(complex_value_type(a + (real_value_type((j * 2) - 1) * h)));
-      }
-
-      const complex_value_type I0 = I;
-      I = (I / real_value_type(2)) + (h * sum);
-
-      const real_value_type ratio = std::abs(I0 / I);
-      const real_value_type delta = std::abs(ratio - real_value_type(1));
-
-      if((k > UINT8_C(1)) && (delta < tol))
+      if((k > integer_value_type(1)) && (delta < tol))
       {
         break;
       }
 
-      n *= 2;
+      n2 *= 2;
 
-      h /= 2;
+      step_size /= 2;
     }
 
-    return I;
+    return integral_result;
   }
 
 #endif // _INTEGRAL_2012_01_09_H_
 
 /*
-#include <cmath>
-#include <complex>
 #include <math/constants/constants.h>
 #include <math/calculus/integral.h>
 
@@ -105,23 +66,28 @@ void do_something();
 
 void do_something()
 {
-  // Compute y = J1(1.23 + 3.45 I) = -1.12043943555101131903 + 3.65329424719653313092 I.
-  // N[BesselJ[2, 123/100 + ((345 / 100) I)], 21]
+  // Compute y = J2(1.23) = 0.16636938378681407351267852431513159437103348245333
+  // N[BesselJ[2, 123/100], 50]
+
+  typedef float float_type;
 
   using std::cos;
   using std::sin;
+  using std::sqrt;
+  using math::constants::pi;
 
-  const std::complex<float> x(1.23F, 3.45F);
-  const std::uint_fast8_t n = 2U;
+  const float_type x = float_type(123) / 100;
 
-  std::complex<float> zj = integral_complex(0.0F,
-                                            math::constants::pi<float>(),
-                                            static_cast<float>(1.0F / 1024),
-                                            [&x, &n](const std::complex<float>& t) -> std::complex<float>
-                                            {
-                                              return cos(x * sin(t) - (float(n) * t));
-                                            }) / math::constants::pi<float>();
+  const std::uint_fast8_t n = 2;
 
-  static_cast<void>(zj);
+  float_type jv = integral(float_type(0),
+                           pi<float_type>(),
+                           sqrt(sqrt(std::numeric_limits<float_type>::epsilon())),
+                           [&x, &n](const float_type& t) -> float_type
+                           {
+                             return cos(x * sin(t) - (float_type(n) * t));
+                           }) / pi<float_type>();
+
+  static_cast<void>(jv);
 }
 */
