11619f9615cab1873ad28aa8dbfcb7feef058186
ckormanyos <e_float@yahoo.com>
2014-11-02 22:18:54 +0100

    Added comments regarding memory index.

--- ref_app/target/micros/bcm2835_raspi_b/startup/int_vect.cpp
+++ ref_app/target/micros/bcm2835_raspi_b/startup/int_vect.cpp
@@ -10,6 +10,7 @@
 #include <iterator>
 #include <mcal_irq.h>
 
+extern "C" void __my_startup                     () __attribute__((section(".text.startup"), used, noinline));
 extern "C" void __int_vect_undef_instr_handler   () __attribute__((section(".text.isr1"),  interrupt("UNDEF"), used, noinline));
 extern "C" void __int_vect_sw_interrupt_handler  () __attribute__((section(".text.isr2"),  interrupt("SWI"),   used, noinline));
 extern "C" void __int_vect_prefetch_abort_handler() __attribute__((section(".text.isr3"),  interrupt("ABORT"), used, noinline));
@@ -26,21 +27,22 @@ extern "C" void __int_vect_future_13_handler     () __attribute__((section(".tex
 extern "C" void __int_vect_future_14_handler     () __attribute__((section(".text.isr14"), interrupt("IRQ"),   used, noinline));
 extern "C" void __int_vect_future_15_handler     () __attribute__((section(".text.isr15"), interrupt("IRQ"),   used, noinline));
 
-void __int_vect_undef_instr_handler   () { mcal::irq::secure::int_vect_undef_instr_handler_callback   (); }
-void __int_vect_sw_interrupt_handler  () { mcal::irq::secure::int_vect_sw_interrupt_handler_callback  (); }
-void __int_vect_prefetch_abort_handler() { mcal::irq::secure::int_vect_prefetch_abort_handler_callback(); }
-void __int_vect_data_abort_handler    () { mcal::irq::secure::int_vect_data_abort_handler_callback    (); }
-void __int_vect_unused_05_handler     () { mcal::irq::secure::int_vect_unused_05_handler_callback     (); }
-void __int_vect_irq_handler           () { mcal::irq::secure::int_vect_irq_handler_callback           (); }
-void __int_vect_fiq_handler           () { mcal::irq::secure::int_vect_fiq_handler_callback           (); }
-void __int_vect_future_08_handler     () { mcal::irq::secure::int_vect_future_08_handler_callback     (); }
-void __int_vect_future_09_handler     () { mcal::irq::secure::int_vect_future_09_handler_callback     (); }
-void __int_vect_future_10_handler     () { mcal::irq::secure::int_vect_future_10_handler_callback     (); }
-void __int_vect_future_11_handler     () { mcal::irq::secure::int_vect_future_11_handler_callback     (); }
-void __int_vect_future_12_handler     () { mcal::irq::secure::int_vect_future_12_handler_callback     (); }
-void __int_vect_future_13_handler     () { mcal::irq::secure::int_vect_future_13_handler_callback     (); }
-void __int_vect_future_14_handler     () { mcal::irq::secure::int_vect_future_14_handler_callback     (); }
-void __int_vect_future_15_handler     () { mcal::irq::secure::int_vect_future_15_handler_callback     (); }
+//   __my_startup                     () : This is implemented in crt0.cpp.                                     Position 00.
+void __int_vect_undef_instr_handler   () { mcal::irq::secure::int_vect_undef_instr_handler_callback   (); }  // Position 01.
+void __int_vect_sw_interrupt_handler  () { mcal::irq::secure::int_vect_sw_interrupt_handler_callback  (); }  // Position 02.
+void __int_vect_prefetch_abort_handler() { mcal::irq::secure::int_vect_prefetch_abort_handler_callback(); }  // Position 03.
+void __int_vect_data_abort_handler    () { mcal::irq::secure::int_vect_data_abort_handler_callback    (); }  // Position 04.
+void __int_vect_unused_05_handler     () { mcal::irq::secure::int_vect_unused_05_handler_callback     (); }  // Position 05.
+void __int_vect_irq_handler           () { mcal::irq::secure::int_vect_irq_handler_callback           (); }  // Position 06.
+void __int_vect_fiq_handler           () { mcal::irq::secure::int_vect_fiq_handler_callback           (); }  // Position 07.
+void __int_vect_future_08_handler     () { mcal::irq::secure::int_vect_future_08_handler_callback     (); }  // Position 08.
+void __int_vect_future_09_handler     () { mcal::irq::secure::int_vect_future_09_handler_callback     (); }  // Position 09.
+void __int_vect_future_10_handler     () { mcal::irq::secure::int_vect_future_10_handler_callback     (); }  // Position 10.
+void __int_vect_future_11_handler     () { mcal::irq::secure::int_vect_future_11_handler_callback     (); }  // Position 11.
+void __int_vect_future_12_handler     () { mcal::irq::secure::int_vect_future_12_handler_callback     (); }  // Position 12.
+void __int_vect_future_13_handler     () { mcal::irq::secure::int_vect_future_13_handler_callback     (); }  // Position 13.
+void __int_vect_future_14_handler     () { mcal::irq::secure::int_vect_future_14_handler_callback     (); }  // Position 14.
+void __int_vect_future_15_handler     () { mcal::irq::secure::int_vect_future_15_handler_callback     (); }  // Position 15.
 
 namespace int_vect
 {
@@ -58,27 +60,28 @@ namespace int_vect
     // from the hex mask and has been transcribed to the table
     // of hex-values below.
 
-    if(load_address == UINT32_C(0))
+    static_cast<void>(load_address);
+
+    const std::uint32_t my_load_address_zero = UINT32_C(0);
+
+    // Here the input parameter "load_address" is checked to be
+    // identically equal to zero. In this sense the input parameter
+    // "load_address" is unused. We supply this parameter in order
+    // to avoid hard-coding a pointer to zero, which would otherwise
+    // result in undesired compiler warnings related to zero used
+    // instead of nullptr.
+
+    static const std::uint8_t the_lower_interrupt_vector_table_data[64U] =
     {
-      // Here the input parameter "load_address" is checked to be
-      // identically equal to zero. In this sense the input parameter
-      // "load_address" is unused. We supply this parameter in order
-      // to avoid hard-coding a pointer to zero, which would otherwise
-      // result in undesired compiler warnings related to zero used
-      // instead of nullptr.
+      0x18U, 0xF0U, 0x9FU, 0xE5U, 0x18U, 0xF0U, 0x9FU, 0xE5U, 0x18U, 0xF0U, 0x9FU, 0xE5U, 0x18U, 0xF0U, 0x9FU, 0xE5U,
+      0x18U, 0xF0U, 0x9FU, 0xE5U, 0x18U, 0xF0U, 0x9FU, 0xE5U, 0x18U, 0xF0U, 0x9FU, 0xE5U, 0x18U, 0xF0U, 0x9FU, 0xE5U,
+      0x00U, 0x84U, 0x00U, 0x00U, 0x40U, 0x80U, 0x00U, 0x00U, 0x80U, 0x80U, 0x00U, 0x00U, 0xC0U, 0x80U, 0x00U, 0x00U,
+      0x00U, 0x81U, 0x00U, 0x00U, 0x40U, 0x81U, 0x00U, 0x00U, 0x80U, 0x81U, 0x00U, 0x00U, 0xC0U, 0x81U, 0x00U, 0x00U,
+    };
 
-      static const std::uint8_t the_lower_interrupt_vector_table_data[64U] =
-      {
-        0x18U, 0xF0U, 0x9FU, 0xE5U, 0x18U, 0xF0U, 0x9FU, 0xE5U, 0x18U, 0xF0U, 0x9FU, 0xE5U, 0x18U, 0xF0U, 0x9FU, 0xE5U,
-        0x18U, 0xF0U, 0x9FU, 0xE5U, 0x18U, 0xF0U, 0x9FU, 0xE5U, 0x18U, 0xF0U, 0x9FU, 0xE5U, 0x18U, 0xF0U, 0x9FU, 0xE5U,
-        0x00U, 0x84U, 0x00U, 0x00U, 0x40U, 0x80U, 0x00U, 0x00U, 0x80U, 0x80U, 0x00U, 0x00U, 0xC0U, 0x80U, 0x00U, 0x00U,
-        0x00U, 0x81U, 0x00U, 0x00U, 0x40U, 0x81U, 0x00U, 0x00U, 0x80U, 0x81U, 0x00U, 0x00U, 0xC0U, 0x81U, 0x00U, 0x00U,
-      };
-
-      // Perform the copy of the lower interrupt vector table data.
-      std::copy(std::begin(the_lower_interrupt_vector_table_data),
-                std::end  (the_lower_interrupt_vector_table_data),
-                reinterpret_cast<volatile std::uint8_t*>(load_address));
-    }
+    // Perform the copy of the lower interrupt vector table data.
+    std::copy(std::begin(the_lower_interrupt_vector_table_data),
+              std::end  (the_lower_interrupt_vector_table_data),
+              reinterpret_cast<volatile std::uint8_t*>(my_load_address_zero));
   }
 }
