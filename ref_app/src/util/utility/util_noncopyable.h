f1791046d6cc5c51da8603f937fd8426084d3999
ckormanyos <ckormanyos@yahoo.com>
2018-06-12 21:23:26 +0200

    Do some trivial cleanup.

--- ref_app/src/util/utility/util_noncopyable.h
+++ ref_app/src/util/utility/util_noncopyable.h
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////
-//  Copyright Christopher Kormanyos 2007 - 2014.
+//  Copyright Christopher Kormanyos 2007 - 2018.
 //  Distributed under the Boost Software License,
 //  Version 1.0. (See accompanying file LICENSE_1_0.txt
 //  or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -17,30 +17,17 @@
       class noncopyable
       {
       protected:
-
-      #if defined(__GNUC__)
-        constexpr noncopyable() = default;
+        noncopyable() = default;
         ~noncopyable() = default;
-      #else
-        noncopyable() {}
-        ~noncopyable() {}
-      #endif
 
       private:
-
-      #if defined(__GNUC__)
         // Emphasize: The following members are private.
         noncopyable(const noncopyable&) = delete;
         noncopyable& operator=(const noncopyable&) = delete;
-      #else
-        // Emphasize: The following members are private.
-        noncopyable(const noncopyable&);
-        noncopyable& operator=(const noncopyable&);
-      #endif
       };
     }
 
-    typedef my_noncopyable_namespace::noncopyable noncopyable;
+    using noncopyable = my_noncopyable_namespace::noncopyable;
   }
 
 #endif // UTIL_NONCOPYABLE_2008_12_16_H_
