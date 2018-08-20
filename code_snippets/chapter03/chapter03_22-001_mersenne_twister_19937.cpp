399a73159378c082cb53d169933834a08054636d
ckormanyos <ckormanyos@yahoo.com>
2018-08-20 21:43:54 +0200

    Improve an example.

--- code_snippets/chapter03/chapter03_22-001_mersenne_twister_19937.cpp
+++ code_snippets/chapter03/chapter03_22-001_mersenne_twister_19937.cpp
@@ -14,19 +14,18 @@
 #include <iostream>
 #include <random>
 
-namespace local
+namespace detail
 {
   // Create a custom random device for seeding the pseudo-random number generator.
   // Use a custom random device because the default is allowed to repeat values.
-  class my_custom_random_device
+  class random_integral_device
   {
   public:
-    typedef std::uint32_t result_type;
+    typedef std::uint64_t result_type;
 
-    my_custom_random_device()
-    {
-      operator()();
-    }
+    random_integral_device() { }
+
+    ~random_integral_device() = default;
 
     static result_type (min)() { return (std::numeric_limits<result_type>::min)(); }
     static result_type (max)() { return (std::numeric_limits<result_type>::max)(); }
@@ -35,35 +34,33 @@ namespace local
     {
       // Use the high-resolution clock to create the basis for a seed.
 
-      static std::uint64_t previous_now_as_integral_value;
-
       using clock_type = std::chrono::high_resolution_clock;
 
-      std::uint64_t current_now_as_integral_value;
+      const std::uint64_t previous_now = 
+        static_cast<std::uint64_t>(std::chrono::duration_cast<std::chrono::nanoseconds>(clock_type::now().time_since_epoch()).count());
 
-      // Wait for std::chrono::high_resolution_clock::now()
-      // to differ from the last call of the seed.
+      std::uint64_t current_now;
+
+      // Wait for std::chrono::high_resolution_clock::now() to differ from the previous value.
       for(;;)
       {
-        current_now_as_integral_value = 
-          std::chrono::duration_cast<std::chrono::nanoseconds>(clock_type::now().time_since_epoch()).count();
+        current_now =
+          static_cast<std::uint64_t>(std::chrono::duration_cast<std::chrono::nanoseconds>(clock_type::now().time_since_epoch()).count());
 
-        if(previous_now_as_integral_value != current_now_as_integral_value)
+        if(current_now != previous_now)
         {
           break;
         }
       }
 
-      previous_now_as_integral_value = current_now_as_integral_value;
-
       // The high-resolution clock is basically a timer counting up.
-      // It could be somewhat deterministic and might suffer from entropy loss.
+      // As this could be slightly deterministic, it might have low entropy.
       // We therefore run a simple CRC (in this case a bitwise CRC-64/WE)
-      // over current_now_as_integral_value to create the seed.
+      // over current_now to create the seed.
 
       // Run CRC-64/WE over the data bytes of current_now_as_integral_value.
 
-      // CRC64 / WE
+      // CRC-64/WE
       // width   : 64
       // poly    : 0x42F0E1EBA9EA3693
       // init    : 0xFFFFFFFFFFFFFFFF
@@ -76,14 +73,14 @@ namespace local
 
       const std::uint64_t data[8U] =
       {
-        std::uint8_t(current_now_as_integral_value >>  0),
-        std::uint8_t(current_now_as_integral_value >>  8),
-        std::uint8_t(current_now_as_integral_value >> 16),
-        std::uint8_t(current_now_as_integral_value >> 24),
-        std::uint8_t(current_now_as_integral_value >> 32),
-        std::uint8_t(current_now_as_integral_value >> 40),
-        std::uint8_t(current_now_as_integral_value >> 48),
-        std::uint8_t(current_now_as_integral_value >> 56),
+        std::uint8_t(current_now >>  0),
+        std::uint8_t(current_now >>  8),
+        std::uint8_t(current_now >> 16),
+        std::uint8_t(current_now >> 24),
+        std::uint8_t(current_now >> 32),
+        std::uint8_t(current_now >> 40),
+        std::uint8_t(current_now >> 48),
+        std::uint8_t(current_now >> 56),
       };
 
       // Initialize the CRC-64/WE.
@@ -97,7 +94,7 @@ namespace local
 
         for(std::uint_fast8_t bit = 8U; bit > 0U; --bit)
         {
-          if(std::uint64_t(crc64_we_value & (1ULL << 63)) != 0U)
+          if(std::uint64_t(crc64_we_value & (UINT64_C(1) << 63)) != 0U)
           {
             crc64_we_value = (crc64_we_value << 1) ^ UINT64_C(0x42F0E1EBA9EA3693);
           }
@@ -111,32 +108,34 @@ namespace local
       // Finalize the CRC-64/WE.
       crc64_we_value ^= UINT64_C(0xFFFFFFFFFFFFFFFF);
 
-      const result_type seed32 = static_cast<result_type>(crc64_we_value);
-
-      std::cout << "Seed is: 0x"
-                << std::hex
-                << std::setw(8)
-                << std::setfill(char('0'))
-                << std::uppercase
-                << seed32
-                << std::endl;
+      const result_type seed_result = crc64_we_value;
 
       // We now have a unique seed expressed as an integral value.
-      return seed32;
+      return seed_result;
     }
 
-    my_custom_random_device(const my_custom_random_device&) = delete;
-    void operator=(const my_custom_random_device&) = delete;
+    random_integral_device(const random_integral_device&) = delete;
+    void operator=(const random_integral_device&) = delete;
   };
 }
 
 void do_something()
 {
-  local::my_custom_random_device               my_random_device;
-  std::mt19937                                 my_generator(my_random_device());
-  std::uniform_int_distribution<std::uint16_t> my_distribution(1, 1023);
+        detail::random_integral_device               my_random_device;
+  const std::uint64_t                                my_random_seed_value(my_random_device());
+        std::mt19937                                 my_generator        (static_cast<std::uint32_t>(my_random_seed_value));
+        std::uniform_int_distribution<std::uint16_t> my_distribution     (1, 1023);
 
-  // Generate 3 pseudo-random numbers.
+  // Print the seed.
+  std::cout << "Seed is: 0x"
+            << std::hex
+            << std::setw(16)
+            << std::setfill(char('0'))
+            << std::uppercase
+            << my_random_seed_value
+            << ". ";
+
+  // Generate 3 pseudo-random numbers in [1, 1023].
   const std::uint16_t random_numbers[3U] =
   {
     my_distribution(my_generator),
@@ -144,17 +143,16 @@ void do_something()
     my_distribution(my_generator)
   };
 
-  std::cout << "Random numbers in [1...1023): ";
+  std::cout << "Random numbers in [1, 1023]: ";
   std::cout << std::dec << std::setw(5) << std::setfill(char(' ')) << random_numbers[0U] << ", ";
   std::cout << std::dec << std::setw(5) << std::setfill(char(' ')) << random_numbers[1U] << ", ";
   std::cout << std::dec << std::setw(5) << std::setfill(char(' ')) << random_numbers[2U] << std::endl;
-  std::cout << std::endl;
 }
 
 int main()
 {
-  // Generate 10 sequences of 3 pseudo-random numbers.
-  for(std::uint_fast8_t i = 0U; i < 10U; ++i)
+  // Generate 20 sequences of 3 pseudo-random numbers.
+  for(std::uint_fast8_t i = 0U; i < 20U; ++i)
   {
     do_something();
   }
