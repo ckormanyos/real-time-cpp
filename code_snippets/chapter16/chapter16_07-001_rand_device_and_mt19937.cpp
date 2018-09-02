82b63a6715d1a942dcda4a5b12fbe84a6d08d5b3
ckormanyos <ckormanyos@yahoo.com>
2018-09-02 11:15:25 +0200

    Continue improving a code snippet.

--- code_snippets/chapter16/chapter16_07-001_rand_device_and_mt19937.cpp
+++ code_snippets/chapter16/chapter16_07-001_rand_device_and_mt19937.cpp
@@ -5,56 +5,79 @@
 //  or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
-// chapter03_22-001_mersenne_twister_19937.cpp
+// chapter16_07-001_rand_device_and_mt19937.cpp
 
 #include <algorithm>
 #include <chrono>
 #include <cstdint>
 #include <iomanip>
 #include <iostream>
+#include <limits>
 #include <random>
+
+// Create a random unsigned custom device
+// for seeding a random number generator.
 
 namespace detail
 {
-  // Create a random unsigned custom device
-  // for seeding the pseudo-random number generator.
+  template<typename unsigned_integral_type>
   class random_unsigned_custom_device final
   {
   public:
-    typedef std::uint64_t result_type;
+    // Use std::numeric_limits for these static asserts instead of
+    // type_traits because potential UDTs are more likely to support
+    // numeric_limits rather than type_traits.
+    static_assert(std::numeric_limits<unsigned_integral_type>::is_specialized == true,
+                  "Error: The unsigned_integral_type parameter must support a template specialization of std::numeric_limits.");
+
+    static_assert((   (std::numeric_limits<unsigned_integral_type>::is_integer == true)
+                   && (std::numeric_limits<unsigned_integral_type>::is_signed  == false)),
+                  "Error: The unsigned_integral_type parameter must be an unsigned integral type.");
+
+    static_assert(std::numeric_limits<unsigned_integral_type>::digits < 512,
+                  "Error: The unsigned_integral_type parameter must have 512 digits or less.");
+
+    using value_type = unsigned_integral_type;
 
     random_unsigned_custom_device() = default;
 
     ~random_unsigned_custom_device() = default;
 
-    static result_type (min)() { return (std::numeric_limits<result_type>::min)(); }
-    static result_type (max)() { return (std::numeric_limits<result_type>::max)(); }
+    static value_type (min)() { return (std::numeric_limits<value_type>::min)(); }
+    static value_type (max)() { return (std::numeric_limits<value_type>::max)(); }
 
     double entropy() const
     {
       // This device uses an algorithmic scheme to produce a seed.
-      // As such it is deterministic, assuming that the algorithm
-      // could be known somehow.
-      return 0.0;
+      // As such it can be considered somewhat deterministic since
+      // the algorithm could be known or possibly guessed.
+
+      // We therefore assign an empirically estimated entropy of 4.0.
+      return 4.0;
     }
 
-    result_type operator()()
+    value_type operator()()
     {
-      // Use the high-resolution clock to create the basis for a seed.
+      // Use the high-resolution clock as a starting point
+      // for creating a unique seed. It will also be hashed
+      // further below.
 
       using clock_type = std::chrono::high_resolution_clock;
 
-      const std::uint64_t previous_now = 
-        static_cast<std::uint64_t>(std::chrono::duration_cast<std::chrono::nanoseconds>
+      // Obtain an initial value of std::chrono::high_resolution_clock::now().
+      const std::uintmax_t previous_now = 
+        static_cast<std::uintmax_t>(std::chrono::duration_cast<std::chrono::nanoseconds>
           (clock_type::now().time_since_epoch()).count());
 
-      std::uint64_t current_now;
+      std::uintmax_t current_now;
 
-      // Wait for std::chrono::high_resolution_clock::now() to differ from the previous value.
       for(;;)
       {
+        // Wait for std::chrono::high_resolution_clock::now()
+        // to differ from the initial value previously obtained.
+
         current_now =
-          static_cast<std::uint64_t>(std::chrono::duration_cast<std::chrono::nanoseconds>
+          static_cast<std::uintmax_t>(std::chrono::duration_cast<std::chrono::nanoseconds>
             (clock_type::now().time_since_epoch()).count());
 
         if(current_now != previous_now)
@@ -63,10 +86,15 @@ namespace detail
         }
       }
 
+      // Finally perform a logical xor of current_now
+      // with the value of all bits set.
+      current_now ^= (std::numeric_limits<std::uintmax_t>::max)();
+
       // The high-resolution clock is basically a timer counting up.
       // As this could be slightly deterministic, it might have low entropy.
-      // We therefore run a simple CRC (in this case a bitwise CRC-64/WE)
-      // over current_now to create the seed.
+      // We therefore attempt to improve entropy by running
+      // a simple CRC over the individual bytes of current_now
+      // in order to create a more unique seed.
 
       // Run CRC-64/WE over the data bytes of current_now_as_integral_value.
 
@@ -118,9 +146,10 @@ namespace detail
       // Finalize the CRC-64/WE.
       crc64_we_value ^= UINT64_C(0xFFFFFFFFFFFFFFFF);
 
-      const result_type seed_result = crc64_we_value;
+      const value_type seed_result = static_cast<value_type>(crc64_we_value);
 
-      // We now have a unique seed expressed as a 64-bit unsigned integral value.
+      // We now have a relatively unique seed expressed
+      // as an unsigned integral value.
       return seed_result;
     }
 
@@ -131,10 +160,11 @@ namespace detail
 
 void do_something()
 {
-        detail::random_unsigned_custom_device        device;
-  const std::uint32_t                                seed(static_cast<std::uint32_t>(device()));
-        std::mt19937                                 generator(seed);
-        std::uniform_int_distribution<std::uint16_t> distribution(1, 1023);
+  detail::random_unsigned_custom_device<std::uint32_t> device;
+  const std::uint32_t seed(static_cast<std::uint32_t>(device()));
+
+  std::mt19937                                 generator(seed);
+  std::uniform_int_distribution<std::uint16_t> distribution(1, 1023);
 
   // Print the seed.
   std::cout << "Seed is: 0x"
@@ -161,9 +191,13 @@ void do_something()
 
 int main()
 {
-  // Generate 20 sequences of 3 pseudo-random numbers.
   for(std::uint_fast8_t i = 0U; i < 20U; ++i)
   {
+    // Generate 20 sequences of 3 pseudo-random numbers.
+    // Here we also generate a new seed in each function call.
+    // In other use cases, a seed might only be generated
+    // once during initialization.
+
     do_something();
   }
 }
