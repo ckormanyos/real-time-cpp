696a5b390f03f836a6d042636581b5de8099502a
ckormanyos <ckormanyos@yahoo.com>
2019-02-24 12:35:33 +0100

    Preliminary progress on xtensa32.

--- ref_app/target/micros/xtensa32/startup/crt1.cpp
+++ ref_app/target/micros/xtensa32/startup/crt1.cpp
@@ -5,6 +5,21 @@
 //  or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
+#include <algorithm>
+#include <iterator>
+
+extern "C"
+{
+  struct ctor_type
+  {
+    typedef void(*function_type)();
+    typedef std::reverse_iterator<const function_type*> const_reverse_iterator;
+  };
+
+  extern ctor_type::function_type _ctors_end[];
+  extern ctor_type::function_type _ctors_begin[];
+}
+
 namespace crt
 {
   void init_ctors();
@@ -12,4 +27,10 @@ namespace crt
 
 void crt::init_ctors()
 {
+  std::for_each(ctor_type::const_reverse_iterator(_ctors_end),
+                ctor_type::const_reverse_iterator(_ctors_begin),
+                [](const ctor_type::function_type pf)
+                {
+                  pf();
+                });
 }
