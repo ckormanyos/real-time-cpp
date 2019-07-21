1a308073f15e53eafe6256ec324ba2b9cfd986b6
ckormanyos <ckormanyos@yahoo.com>
2019-07-21 22:44:59 +0200

    Add target x86_64-w64-mingw32.

--- ref_app/src/mcal/win32/mcal_gpt.cpp
+++ ref_app/src/mcal/win32/mcal_gpt.cpp
@@ -11,21 +11,29 @@
 
 #include <mcal_gpt.h>
 
-mcal::gpt::value_type mcal::gpt::secure::get_time_elapsed()
+namespace
 {
   using mcal_gpt_time_point_type =
     std::chrono::high_resolution_clock::time_point;
 
-  static mcal_gpt_time_point_type mcal_gpt_time_point_init =
-    std::chrono::high_resolution_clock::now();
-
-  // Sleep in order to reduce the load on a Windows simulated target.
-  static std::uint_fast16_t prescale;
-
-  ++prescale;
-
-  if((prescale % UINT16_C(8192)) == 0U)
+  static const mcal_gpt_time_point_type& mcal_gpt_time_point_init()
   {
+    static const mcal_gpt_time_point_type init =
+      std::chrono::high_resolution_clock::now();
+
+    return init;
+  }
+
+  std::uint_fast16_t mcal_gpt_sleep_prescaler;
+}
+
+mcal::gpt::value_type mcal::gpt::secure::get_time_elapsed()
+{
+  ++mcal_gpt_sleep_prescaler;
+
+  if((mcal_gpt_sleep_prescaler % UINT16_C(8192)) == 0U)
+  {
+    // Sleep in order to reduce the load on a PC target.
     std::this_thread::sleep_for(std::chrono::milliseconds(3U));
   }
   else
@@ -33,10 +41,10 @@ mcal::gpt::value_type mcal::gpt::secure::get_time_elapsed()
     std::this_thread::yield();
   }
 
-  const auto local_duration_in_microseconds =
+  const std::chrono::microseconds duration_in_microseconds =
     std::chrono::duration_cast<std::chrono::microseconds>
-      (std::chrono::high_resolution_clock::now() - mcal_gpt_time_point_init);
+      (std::chrono::high_resolution_clock::now() - mcal_gpt_time_point_init());
 
   // Return the system tick with a resolution of 1us.
-  return static_cast<mcal::gpt::value_type>(local_duration_in_microseconds.count());
+  return static_cast<mcal::gpt::value_type>(duration_in_microseconds.count());
 }
