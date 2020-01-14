9d5905b2549cf9f9ce66ceb25c0df4853337643e
Christopher Kormanyos <ckormanyos@yahoo.com>
2020-01-14 22:12:13 +0100

    Update example 12_04 benchmark

--- examples/chapter12_04/src/app/benchmark/app_benchmark.cpp
+++ examples/chapter12_04/src/app/benchmark/app_benchmark.cpp
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////
-//  Copyright Christopher Kormanyos 2017 - 2018.
+//  Copyright Christopher Kormanyos 2017 - 2020.
 //  Distributed under the Boost Software License,
 //  Version 1.0. (See accompanying file LICENSE_1_0.txt
 //  or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -67,62 +67,37 @@ void app::benchmark::task_init()
 
 void app::benchmark::task_func()
 {
-  app_benchmark_result_is_ok = true;
+  static std::uint_fast8_t app_benchmark_index;
 
-  // Disable all interrupts before each calculation
-  // in order to provide for a clean time measurement.
-  mcal::irq::disable_all();
+  app_benchmark_result_is_ok = true;
 
   // Use a port pin to provide a real-time measurement.
   app_benchmark_port_type::set_pin_high();
 
-  // Test the value of a Bessel function.
-  // Here is a control value from Wolfram Alpha.
-  // N[BesselJ[11/9, EulerGamma], 40]
-  // 0.1890533651853886085356717332711858771597
-
+  if(app_benchmark_index == 0U)
   {
-    const std::float32_t v = FLOAT32_C(11.0) / FLOAT32_C(9.0);
+    // Test the value of a Bessel function.
+    // Here is a control value from Wolfram Alpha.
+    // N[BesselJ[11/9, EulerGamma], 40]
+    // 0.1890533651853886085356717332711858771597
+
+    constexpr std::float32_t v = FLOAT32_C(11.0) / FLOAT32_C(9.0);
 
     app_benchmark_result_bessel =
       math::functions::cyl_bessel_j(v, math::constants::euler<std::float32_t>());
+
+    app_benchmark_result_is_ok &= is_close_fraction(FLOAT32_C(0.1890533652),
+                                                    app_benchmark_result_bessel,
+                                                    app_benchmark_tolerance);
   }
-
-  app_benchmark_result_is_ok &= is_close_fraction(FLOAT32_C(0.1890533652),
-                                                  app_benchmark_result_bessel,
-                                                  app_benchmark_tolerance);
-
-  if(app_benchmark_result_is_ok)
+  else if(app_benchmark_index == 1U)
   {
-    app_benchmark_port_type::set_pin_low();
+    // Test the value of a hypergeometric function.
+    // Here is a control value from Wolfram Alpha.
+    // N[HypergeometricPFQ[3/{7, 8, 9, 10}, 7/{13, 14, 15, 16, 17}, Log[2]], 40]
+    // 1.583596313998374915091256357139915173598
 
-    // Remember to enable all interrupts after the calculation.
-    mcal::irq::enable_all();
-  }
-  else
-  {
-    // The result of this floating-point benchmark is not OK.
-
-    // Crash the system and toggle a port to provide an indication of failure.
-    for(;;) { app_benchmark_port_type::toggle_pin(); mcal::cpu::nop(); }
-  }
-
-  app_benchmark_timer_type::blocking_delay(app_benchmark_timer_type::microseconds(25U));
-
-  // Disable all interrupts before each calculation
-  // in order to provide for a clean time measurement.
-  mcal::irq::disable_all();
-
-  // Use a port pin to provide a real-time measurement.
-  app_benchmark_port_type::set_pin_high();
-
-  // Test the value of a hypergeometric function.
-  // Here is a control value from Wolfram Alpha.
-  // N[HypergeometricPFQ[3/{7, 8, 9, 10}, 7/{13, 14, 15, 16, 17}, Log[2]], 40]
-  // 1.583596313998374915091256357139915173598
-
-  {
-    const std::array<std::float32_t, 4U> ap =
+    constexpr std::array<std::float32_t, 4U> ap =
     {{
       FLOAT32_C(3.0) / FLOAT32_C( 7.0),
       FLOAT32_C(3.0) / FLOAT32_C( 8.0),
@@ -130,7 +105,7 @@ void app::benchmark::task_func()
       FLOAT32_C(3.0) / FLOAT32_C(10.0)
     }};
 
-    const std::array<std::float32_t, 5U> bq =
+    constexpr std::array<std::float32_t, 5U> bq =
     {{
       FLOAT32_C(7.0) / FLOAT32_C(13.0),
       FLOAT32_C(7.0) / FLOAT32_C(14.0),
@@ -145,61 +120,39 @@ void app::benchmark::task_func()
                                           bq.cbegin(),
                                           bq.cend(),
                                           math::constants::ln_two<std::float32_t>());
+
+    app_benchmark_result_is_ok &= is_close_fraction(FLOAT32_C(1.5835963140),
+                                                    app_benchmark_result_hypergeometric,
+                                                    app_benchmark_tolerance);
   }
-
-  app_benchmark_result_is_ok &= is_close_fraction(FLOAT32_C(1.5835963140),
-                                                  app_benchmark_result_hypergeometric,
-                                                  app_benchmark_tolerance);
-
-  if(app_benchmark_result_is_ok)
+  else if(app_benchmark_index == 2U)
   {
-    app_benchmark_port_type::set_pin_low();
-
-    // Remember to enable all interrupts after the calculation.
-    mcal::irq::enable_all();
-  }
-  else
-  {
-    // The result of this floating-point benchmark is not OK.
-
-    // Crash the system and toggle a port to provide an indication of failure.
-    for(;;) { app_benchmark_port_type::toggle_pin(); mcal::cpu::nop(); }
-  }
-
-  app_benchmark_timer_type::blocking_delay(app_benchmark_timer_type::microseconds(25U));
-
-  // Disable all interrupts before each calculation
-  // in order to provide for a clean time measurement.
-  mcal::irq::disable_all();
-
-  // Use a port pin to provide a real-time measurement.
-  app_benchmark_port_type::set_pin_high();
-
-  // Test the value of a Legendre function of the first kind.
-  // Here is a control value from Wolfram Alpha.
-  // N[LegendreP[1/11, 14/19, 2/7], 40]
-  // 0.2937838815278435137954432141091105343408
-
-  {
-    const std::float32_t v = FLOAT32_C( 1.0) / FLOAT32_C(11.0);
-    const std::float32_t u = FLOAT32_C(14.0) / FLOAT32_C(19.0);
-    const std::float32_t x = FLOAT32_C( 2.0) / FLOAT32_C( 7.0);
+    // Test the value of a Legendre function of the first kind.
+    // Here is a control value from Wolfram Alpha.
+    // N[LegendreP[1/11, 14/19, 2/7], 40]
+    // 0.2937838815278435137954432141091105343408
+    constexpr std::float32_t v = FLOAT32_C( 1.0) / FLOAT32_C(11.0);
+    constexpr std::float32_t u = FLOAT32_C(14.0) / FLOAT32_C(19.0);
+    constexpr std::float32_t x = FLOAT32_C( 2.0) / FLOAT32_C( 7.0);
 
     app_benchmark_result_legendre = math::functions::legendre_p(v, u, x);
+
+    app_benchmark_result_is_ok &= is_close_fraction(FLOAT32_C(0.2937838815),
+                                                    app_benchmark_result_legendre,
+                                                    app_benchmark_tolerance);
   }
 
-  app_benchmark_result_is_ok &= is_close_fraction(FLOAT32_C(0.2937838815),
-                                                  app_benchmark_result_legendre,
-                                                  app_benchmark_tolerance);
+  app_benchmark_port_type::set_pin_low();
 
-  if(app_benchmark_result_is_ok)
+  ++app_benchmark_index;
+
+  if(app_benchmark_index == 3U)
   {
-    app_benchmark_port_type::set_pin_low();
-
-    // Remember to enable all interrupts after the calculation.
-    mcal::irq::enable_all();
+    app_benchmark_index = 0U;
   }
-  else
+
+
+  if(app_benchmark_result_is_ok == false)
   {
     // The result of this floating-point benchmark is not OK.
 
