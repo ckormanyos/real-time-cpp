ee00c0d4488f15647bde5fa185eccf110775cff6
Christopher Kormanyos <ckormanyos@yahoo.com>
2020-01-15 05:41:59 +0100

    Add more readme details

--- examples/chapter09_07/src/app/display/app_display.cpp
+++ examples/chapter09_07/src/app/display/app_display.cpp
@@ -1,6 +1,6 @@
 
 ///////////////////////////////////////////////////////////////////////////////
-//  Copyright Christopher Kormanyos 2015.
+//  Copyright Christopher Kormanyos 2015 - 2020.
 //  Distributed under the Boost Software License,
 //  Version 1.0. (See accompanying file LICENSE_1_0.txt
 //  or copy at http://www.boost.org/LICENSE_1_0.txt)
