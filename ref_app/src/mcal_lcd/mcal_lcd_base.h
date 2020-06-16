6ba11631f731a7e8b189322956678c3931bb1e15
Christopher Kormanyos <ckormanyos@yahoo.com>
2020-06-16 22:30:52 +0200

    Add some not yet used lcd classes.

---
+++ ref_app/src/mcal_lcd/mcal_lcd_base.h
@@ -0,0 +1,27 @@
+#ifndef MCAL_LCD_BASE_2020_06_10_H_
+  #define MCAL_LCD_BASE_2020_06_10_H_
+
+  #include <cstdint>
+
+  namespace mcal { namespace lcd {
+
+  class lcd_base
+  {
+  public:
+    virtual ~lcd_base() = default;
+
+    virtual bool init() = 0;
+
+    virtual bool write(const char* pstr,
+                       const std::uint_fast8_t length,
+                       const std::uint_fast8_t line_index = 0) = 0;
+
+  protected:
+    lcd_base() = default;
+
+    virtual bool set_line_index() { return true; }
+  };
+
+  } } // namespace mcal::lcd
+
+#endif // MCAL_LCD_BASE_2020_06_10_H_
