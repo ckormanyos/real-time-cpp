edf46bcfdd1bfe11c9be58d8cef49b6d14461439
Christopher Kormanyos <ckormanyos@yahoo.com>
2020-08-08 08:53:28 +0200

    Do minor cleanup of copyright dates Pi Zero

--- ref_app/target/build/build.bat
+++ ref_app/target/build/build.bat
@@ -1,5 +1,5 @@
 @rem
-@rem  Copyright Christopher Kormanyos 2007 - 2018.
+@rem  Copyright Christopher Kormanyos 2007 - 2020.
 @rem  Distributed under the Boost Software License,
 @rem  Version 1.0. (See accompanying file LICENSE_1_0.txt
 @rem  or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -12,13 +12,13 @@
 @rem %1: target
 @rem       am335x           : BeagleBone with TI(R) AM3359, creates a bare-metal binary file "MLO"
 @rem       avr              : ATMEL(R) AVR(R) ATmega328P i.e., Arduino compatible
-@rem       bcm2835_raspi_b  : RaspberryPi Model B, creates a bare-metal binary file "kernel.img"
-@rem       pic32mx795fx     : PIC32(R) MIPS
+@rem       bcm2835_raspi_b  : RaspberryPi Zero, creates a bare-metal binary file "kernel.img"
 @rem       rl78             : Renesas(R) RL78
 @rem       rx63n            : Renesas(R) RX600
 @rem       stm32f100        : ST Microelectronics STM32F100 ARM(R) Cortex(TM)-M3, STM32VLDISCOVERY board
 @rem       stm32f407        : ST Microelectronics STM32F407 ARM(R) Cortex(TM)-M4, STM32F4DISCOVERY board
 @rem       stm32f429        : ST Microelectronics STM32F429 ARM(R) Cortex(TM)-M4, 32F429IDISCOVERY board
+@rem       stm32f446        : ST Microelectronics STM32F446 ARM(R) Cortex(TM)-M4, 32F446-Nucleo
 @rem
 @rem %2: buildtype
 @rem                        : None. Build the project. Compile and link outdated files only.
