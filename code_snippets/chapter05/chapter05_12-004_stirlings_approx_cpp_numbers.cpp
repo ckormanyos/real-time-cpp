d8f36b239ec87fd8663b3b6f002f8ce0b622e44f
Christopher Kormanyos <ckormanyos@yahoo.com>
2020-09-14 20:25:50 +0200

    Clean up a few code snippets

--- code_snippets/chapter05/chapter05_12-004_stirlings_approx_cpp_numbers.cpp
+++ code_snippets/chapter05/chapter05_12-004_stirlings_approx_cpp_numbers.cpp
@@ -1,24 +1,17 @@
 ///////////////////////////////////////////////////////////////////////////////
-//  Copyright Christopher Kormanyos 2019- 2020.
+//  Copyright Christopher Kormanyos 2020.
 //  Distributed under the Boost Software License,
 //  Version 1.0. (See accompanying file LICENSE_1_0.txt
 //  or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
-// chapter05_12-003_stirlings_approx.cpp
+// chapter05_12-004_stirlings_approx_cpp_numbers.cpp
 
 #include <cmath>
 #include <iomanip>
 #include <iostream>
 #include <limits>
-
-template<typename T>
-constexpr T pi =
-  T(3.1415926535'8979323846'2643383279'5028841972L);
-
-template<typename T>
-constexpr T e =
-  T(2.7182818284'5904523536'0287471352'6624977572L);
+#include <numbers>
 
 template<typename T>
 T tgamma_order_2(T x)
@@ -29,14 +22,14 @@ T tgamma_order_2(T x)
   const T      one_over_x = T(1.0L) / x;
   const T sqrt_one_over_x = sqrt(one_over_x);
 
-  const T term0 =    sqrt(T(2.0L) * pi<T>)
+  const T term0 =    sqrt(T(2.0L) * std::numbers::pi_v<T>)
                    * sqrt_one_over_x;
-  const T term1 = (  sqrt(pi<T> / T(2.0L))
+  const T term1 = (  sqrt(std::numbers::pi_v<T> / T(2.0L))
                    * (  sqrt_one_over_x
                       * one_over_x))
                   / T(6.0L);
 
-  return pow(x / e<T>, x) * (term0 + term1);
+  return pow(x / std::numbers::e_v<T>, x) * (term0 + term1);
 }
 
 int main()
