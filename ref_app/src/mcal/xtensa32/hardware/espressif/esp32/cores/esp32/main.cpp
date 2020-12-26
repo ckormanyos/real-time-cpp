9a8261886fd5ae5b932d27e5b13e8d9e1b1eba4e
Christopher Kormanyos <ckormanyos@yahoo.com>
2020-12-26 14:05:51 +0100

    Compile ESP32 SDK at first all Arduino framework

---
+++ ref_app/src/mcal/xtensa32/hardware/espressif/esp32/cores/esp32/main.cpp
@@ -0,0 +1,35 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_task_wdt.h"
+#include "Arduino.h"
+
+#ifndef CONFIG_ARDUINO_LOOP_STACK_SIZE
+#define CONFIG_ARDUINO_LOOP_STACK_SIZE 8192
+#endif
+
+TaskHandle_t loopTaskHandle = NULL;
+
+#if CONFIG_AUTOSTART_ARDUINO
+
+bool loopTaskWDTEnabled;
+
+void loopTask(void *pvParameters)
+{
+    setup();
+    for(;;) {
+        if(loopTaskWDTEnabled){
+            esp_task_wdt_reset();
+        }
+        loop();
+        if (serialEventRun) serialEventRun();
+    }
+}
+
+extern "C" void app_main()
+{
+    loopTaskWDTEnabled = false;
+    initArduino();
+    xTaskCreateUniversal(loopTask, "loopTask", CONFIG_ARDUINO_LOOP_STACK_SIZE, NULL, 1, &loopTaskHandle, CONFIG_ARDUINO_RUNNING_CORE);
+}
+
+#endif
