6ada688631ae191ef5a8c80f4b33f9a9f9053582
Christopher Kormanyos <ckormanyos@yahoo.com>
2020-12-26 23:02:51 +0100

    Start removing unused program artifacts

--- ref_app/src/mcal/xtensa32/hardware/espressif/esp32/cores/esp32/main.cpp
+++ ref_app/src/mcal/xtensa32/hardware/espressif/esp32/cores/esp32/main.cpp
@@ -7,29 +7,35 @@
 #define CONFIG_ARDUINO_LOOP_STACK_SIZE 8192
 #endif
 
-TaskHandle_t loopTaskHandle = NULL;
-
-#if CONFIG_AUTOSTART_ARDUINO
+TaskHandle_t loopTaskHandle = nullptr;
 
 bool loopTaskWDTEnabled;
 
 void loopTask(void *pvParameters)
 {
-    setup();
-    for(;;) {
-        if(loopTaskWDTEnabled){
-            esp_task_wdt_reset();
-        }
-        loop();
-        if (serialEventRun) serialEventRun();
+  setup();
+
+  for(;;)
+  {
+    if(loopTaskWDTEnabled)
+    {
+      esp_task_wdt_reset();
     }
+
+    loop();
+  }
 }
 
 extern "C" void app_main()
 {
   loopTaskWDTEnabled = false;
   initArduino();
-  xTaskCreateUniversal(loopTask, "loopTask", CONFIG_ARDUINO_LOOP_STACK_SIZE, NULL, 1, &loopTaskHandle, CONFIG_ARDUINO_RUNNING_CORE);
-}
 
-#endif
+  xTaskCreateUniversal(loopTask,
+                       "loopTask",
+                       CONFIG_ARDUINO_LOOP_STACK_SIZE,
+                       nullptr,
+                       1,
+                       &loopTaskHandle,
+                       CONFIG_ARDUINO_RUNNING_CORE);
+}
