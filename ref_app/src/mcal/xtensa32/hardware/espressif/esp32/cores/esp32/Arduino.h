460cd284bca0d0e284f75ef0521c531b2ecd5690
Christopher Kormanyos <ckormanyos@yahoo.com>
2020-12-27 19:11:42 +0100

    Reduce files of partial ESP32 SDK

--- ref_app/src/mcal/xtensa32/hardware/espressif/esp32/cores/esp32/Arduino.h
+++ ref_app/src/mcal/xtensa32/hardware/espressif/esp32/cores/esp32/Arduino.h
@@ -142,11 +142,6 @@ void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t val);
 #include <algorithm>
 #include <cmath>
 
-#include "Printable.h"
-#include "Print.h"
-#include "IPAddress.h"
-#include "Client.h"
-#include "Server.h"
 #include "Esp.h"
 
 using std::abs;
