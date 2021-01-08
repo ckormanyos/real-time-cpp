46fcfd1f704ffb049c054e1361d5f1fb708c3b44
Christopher Kormanyos <ckormanyos@yahoo.com>
2021-01-08 20:05:01 +0100

    ATmega4809 flags, vector table, LD progress, etc

--- ref_app/src/mcal/atmega4809/mcal_wdg.cpp
+++ ref_app/src/mcal/atmega4809/mcal_wdg.cpp
@@ -10,41 +10,8 @@
 
 void mcal::wdg::init(const config_type*)
 {
-  // Read the MCU status register.
-  volatile const std::uint8_t mcu_status_register =
-    mcal::reg::reg_access_static<std::uint8_t,
-                                 std::uint8_t,
-                                 mcal::reg::mcusr>::reg_get();
-
-  // At the moment, we do not make use of the MCU status register.
-  // In the future, for example, we could query the reset reason.
-  static_cast<void>(mcu_status_register);
-
-  // Clear the MCU status register.
-  mcal::reg::reg_access_static<std::uint8_t,
-                               std::uint8_t,
-                               mcal::reg::mcusr,
-                               std::uint8_t(0U)>::reg_set();
-
-  // Reset the watchdog timer.
-  asm volatile("wdr");
-
-  // Set the watchdog timer period and activate the watchdog timer.
-  mcal::reg::reg_access_static<std::uint8_t,
-                               std::uint8_t,
-                               mcal::reg::wdtcsr,
-                               std::uint8_t(0x18U)>::reg_set();
-
-  // See Chapter 11.9.2, Table 11-2: Watchdog Timer Prescale Select.
-  // Select WDP3:WDP0 in WDTCSR to binary 0b0111, resulting
-  // in a watchdog period of approximately 2s.
-  mcal::reg::reg_access_static<std::uint8_t,
-                               std::uint8_t,
-                               mcal::reg::wdtcsr,
-                               std::uint8_t(0x08U) | std::uint8_t(0x07U)>::reg_set();
 }
 
 void mcal::wdg::secure::trigger()
 {
-  asm volatile("wdr");
 }
--- ref_app/target/micros/atmega4809/startup/int_vect.cpp
+++ ref_app/target/micros/atmega4809/startup/int_vect.cpp
@@ -11,7 +11,7 @@
 
 extern "C" void __my_startup       () __attribute__((section(".startup"), used, noinline));
 extern "C" void __vector_unused_irq() __attribute__((signal, used, externally_visible));
-extern "C" void __vector_16        () __attribute__((signal, used, externally_visible));
+extern "C" void __vector_3         () __attribute__((signal, used, externally_visible));
 
 extern "C"
 void __vector_unused_irq()
@@ -35,36 +35,50 @@ namespace
 }
 
 extern "C"
-const volatile std::array<isr_type, 26U> __isr_vector __attribute__((section(".isr_vector")));
+const volatile std::array<isr_type, 40U> __isr_vector __attribute__((section(".isr_vector")));
 
 extern "C"
-const volatile std::array<isr_type, 26U> __isr_vector =
+const volatile std::array<isr_type, 40U> __isr_vector =
 {{
                                               // addr.  nr. interrupt source
-  { { 0x0C, 0x94 }, __my_startup },           // 0x00,  0,  reset
-  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x02,  1,  ext0
-  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x04,  2,  ext1
-  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x06,  3,  pin0
-  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x08,  4,  pin1
-  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x0A,  5,  pin2
-  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x0C,  6,  watchdog
-  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x0E,  7,  timer2 cmp a
-  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x10,  8,  timer2 cmp b
-  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x12,  9,  timer2 ovf
-  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x14, 10,  timer1 cap
-  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x16, 11,  timer1 cmp a
-  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x18, 12,  timer1 cmp b
-  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x1A, 13,  timer1 ovf
-  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x1C, 14,  timer0 cmp a
-  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x1E, 15,  timer0 cmp b
-  { { 0x0C, 0x94 }, __vector_16 },            // 0x2E, 16,  timer0 ovf
-  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x20, 17,  spi(TM)
-  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x22, 18,  usart rx
-  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x24, 19,  usart err
-  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x26, 20,  usart tx
-  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x28, 21,  adc
-  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x2A, 22,  eep ready
-  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x2C, 23,  comparator
-  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x2E, 24,  two-wire
-  { { 0x0C, 0x94 }, __vector_unused_irq }     // 0x30, 25,  spm
+  { { 0x0C, 0x94 }, __my_startup },           // 0x00 RESET
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x02 NMI    Non-Maskable Interrupt from CRC 2
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x04 VLM    Voltage Level Monitor 3
+  { { 0x0C, 0x94 }, __vector_3 },             // 0x06 RTC    Overflow or compare match 4
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x08 PIT    Periodic interrupt 5
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x0A CCL    Configurable Custom Logic 6
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x0C PORTA  External interrupt 7
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x0E TCA0   Overflow 8
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x10 TCA0   Underflow (Split mode) 9
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x12 TCA0   Compare channel 0 
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x14 TCA0   Compare channel 1 11
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x16 TCA0   Compare channel 2 12
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x18 TCB0   Capture 13
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x1A TCB1   Capture 14
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x1C TWI0   Slave 15
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x1E TWI0   Master 16
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x20 SPI0   Serial Peripheral Interface 0 17
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x22 USART0 Receive Complete 18
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x24 USART0 Data Register Empty 19
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x26 USART0 Transmit Complete 20
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x28 PORTD  External interrupt 21
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x2A AC0    Compare 22
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x2C ADC0   Result Ready 23
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x2E ADC0   Window Compare 24
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x30 PORTC  External interrupt 25
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x32 TCB2   Capture 26
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x34 USART1 Receive Complete 27
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x36 USART1 Data Register Empty 28
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x38 USART1 Transmit Complete 29
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x3A PORTF  External interrupt 30
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x3C NVM    Ready 31
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x3E USART2 Receive Complete 32
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x40 USART2 Data Register Empty 33
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x42 USART2 Transmit Complete 34
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x44 PORTB  External interruptX35
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x46 PORTE  External interrupt 36
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x48 TCB3   Capture 37
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x4A USART3 Receive Complete 38
+  { { 0x0C, 0x94 }, __vector_unused_irq },    // 0x4C USART3 Data Register Empty 39
+  { { 0x0C, 0x94 }, __vector_unused_irq }     // 0x4E USART3 Transmit Complete
 }};
