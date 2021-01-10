60bc7994b839727205b506191e7c33595e0f1790
Christopher Kormanyos <ckormanyos@yahoo.com>
2021-01-10 11:23:50 +0100

    mega4809 optimize port + verify bench beats avr

--- ref_app/src/mcal/atmega4809/mcal_port.h
+++ ref_app/src/mcal/atmega4809/mcal_port.h
@@ -19,48 +19,6 @@
   bool mcal_port_pin_expander_read_input_value    (const uint8_t bpos);
   void mcal_port_pin_expander_toggle_pin          (const uint8_t bpos);
 
-  // From "iom4809.h"
-  // I/O Ports
-  extern "C"
-  {
-    typedef struct PORT_struct
-    {
-      uint8_t DIR;           // Data Direction
-      uint8_t DIRSET;        // Data Direction Set
-      uint8_t DIRCLR;        // Data Direction Clear
-      uint8_t DIRTGL;        // Data Direction Toggle
-      uint8_t OUT;           // Output Value
-      uint8_t OUTSET;        // Output Value Set
-      uint8_t OUTCLR;        // Output Value Clear
-      uint8_t OUTTGL;        // Output Value Toggle
-      uint8_t IN;            // Input Value
-      uint8_t INTFLAGS;      // Interrupt Flags
-      uint8_t PORTCTRL;      // Port Control
-      uint8_t reserved_0x0B;
-      uint8_t reserved_0x0C;
-      uint8_t reserved_0x0D;
-      uint8_t reserved_0x0E;
-      uint8_t reserved_0x0F;
-      uint8_t PIN0CTRL;      // Pin 0 Control
-      uint8_t PIN1CTRL;      // Pin 1 Control
-      uint8_t PIN2CTRL;      // Pin 2 Control
-      uint8_t PIN3CTRL;      // Pin 3 Control
-      uint8_t PIN4CTRL;      // Pin 4 Control
-      uint8_t PIN5CTRL;      // Pin 5 Control
-      uint8_t PIN6CTRL;      // Pin 6 Control
-      uint8_t PIN7CTRL;      // Pin 7 Control
-      uint8_t reserved_0x18;
-      uint8_t reserved_0x19;
-      uint8_t reserved_0x1A;
-      uint8_t reserved_0x1B;
-      uint8_t reserved_0x1C;
-      uint8_t reserved_0x1D;
-      uint8_t reserved_0x1E;
-      uint8_t reserved_0x1F;
-    }
-    PORT_t;
-  }
-
   namespace mcal
   {
     namespace port
@@ -111,19 +69,13 @@
         static void set_pin_high() noexcept
         {
           // Set the port output value to high.
-          mcal::reg::reg_access_static<address_uintptr_type,
-                                       register_value_type,
-                                       pout_set__address,
-                                       bpos_value>::bit_set();
+          *(std::uint8_t*) pout_set__address |= (std::uint8_t) (1U << bpos_value);
         }
 
         static void set_pin_low() noexcept
         {
           // Set the port output value to low.
-          mcal::reg::reg_access_static<address_uintptr_type,
-                                       register_value_type,
-                                       pout_clr__address,
-                                       bpos_value>::bit_set();
+          *(std::uint8_t*) pout_clr__address |= (std::uint8_t) (1U << bpos_value);
         }
 
         static bool read_input_value() noexcept
@@ -138,10 +90,7 @@
         static void toggle_pin() noexcept
         {
           // Toggle the port output value.
-          mcal::reg::reg_access_static<address_uintptr_type,
-                                       register_value_type,
-                                       pout_tgl__address,
-                                       bpos_value>::bit_set();
+          *(std::uint8_t*) pout_tgl__address |= (std::uint8_t) (1U << bpos_value);
         }
       };
 
