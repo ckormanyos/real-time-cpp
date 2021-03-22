0ab8da14fee01b6f4caf3117d585d7513692de1b
Christopher Kormanyos <ckormanyos@yahoo.com>
2021-03-22 16:00:48 +0100

    Try restore all CI builds and runs

--- ref_app/src/app/benchmark/app_benchmark_boost_math_cbrt.cpp
+++ ref_app/src/app/benchmark/app_benchmark_boost_math_cbrt.cpp
@@ -5,6 +5,7 @@
 //  or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
+#include <array>
 #include <cstdint>
 
 #define BOOST_MATH_PROMOTE_FLOAT_POLICY false
@@ -39,16 +40,43 @@ extern float x;
 
 bool app::benchmark::run_boost_math_cbrt()
 {
-  cb = boost::math::tgamma(boost::math::cbrt(x));
+  // Table[N[Gamma[((456 n)/(100 Pi))^(1/3)], 21], {n, 1, 101, 10}]
 
-  // N[Gamma[((123 Pi) / 100)^(1/3)], 21]
-  const bool result_is_ok = local::is_close_fraction(cb, 0.890420918714041301448F);
+  constexpr std::array<float, 11U> app_benchmark_control =
+  {{
+     0.939131814550229181011F,
+     1.34645417009670049451F,
+     2.24865197861464813285F,
+     3.54010862139328036869F,
+     5.32678290474027222700F,
+     7.74480814145513084405F,
+    10.9608033153333194319F,
+    15.1764307769968425577F,
+    20.6341757772658373763F,
+    27.6240717359003789929F,
+    36.4914014381246411895F
+  }};
 
-  return result_is_ok;
+  static unsigned app_benchmark_n_index      = 0U;
+  static unsigned app_benchmark_n_value      = 1U;
+  static bool     app_benchmark_result_is_ok = true;
+
+  cb = boost::math::tgamma(boost::math::cbrt(x * float(app_benchmark_n_value)));
+
+  app_benchmark_result_is_ok &= local::is_close_fraction(cb, app_benchmark_control[app_benchmark_n_index]);
+
+  app_benchmark_n_value += 10U;
+
+  ++app_benchmark_n_index;
+
+  if(app_benchmark_n_index == app_benchmark_control.size())
+  {
+    app_benchmark_n_index = 0U;
+    app_benchmark_n_value = 1U;
+  }
+
+  return app_benchmark_result_is_ok;
 }
-
-float cb;
-float x = 1.23F * boost::math::constants::pi<float>();
 
 #if defined(APP_BENCHMARK_STANDALONE_MAIN)
 int main()
@@ -66,5 +94,8 @@ int main()
 }
 
 #endif
+
+float cb;
+float x = 4.56F / boost::math::constants::pi<float>();
 
 #endif // APP_BENCHMARK_TYPE_BOOST_MATH_CBRT
