4ca1b62e989a28765179da70a234bc5fb6d22dc5
Christopher Kormanyos <ckormanyos@yahoo.com>
2022-04-18 08:16:54 +0200

    Try LGTM integration

--- ref_app/src/sys/start/sys_start.cpp
+++ ref_app/src/sys/start/sys_start.cpp
@@ -1,5 +1,5 @@
-///////////////////////////////////////////////////////////////////////////////
-//  Copyright Christopher Kormanyos 2007 - 2021.
+﻿///////////////////////////////////////////////////////////////////////////////
+//  Copyright Christopher Kormanyos 2007 - 2022.
 //  Distributed under the Boost Software License,
 //  Version 1.0. (See accompanying file LICENSE_1_0.txt
 //  or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -8,15 +8,18 @@
 #include <mcal/mcal.h>
 #include <os/os.h>
 
+// To manually build with GNUmake on *nix:
 // cd /mnt/c/Users/User/Documents/Ks/uC_Software/Boards/real-time-cpp/ref_app
 // make -f target/app/make/app_make.gmk rebuild TGT=x86_64-w64-mingw32
 
+// To manually build with CMake for target host:
 // cd real-time-cpp
 // mkdir -p build
 // cd build
 // cmake ../ref_app -DTARGET=host -DCMAKE_TOOLCHAIN_FILE=../ref_app/cmake/gcc-toolchain.cmake
 // make -j ref_app
 
+// To build the local clang-tidy project on *nix:
 // cd /mnt/c/Users/User/Documents/Ks/uC_Software/Boards/real-time-cpp
 // cd .tidy/make
 // make prepare -f make_tidy_01_generic.gmk
