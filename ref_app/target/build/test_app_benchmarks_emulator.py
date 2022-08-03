a97cfc7bc95389560a37e099d2cae038ce19d7e1
Iliass Mahjoub <aitelmahjoub.iliass@gmail.com>
2022-08-03 19:26:08 +0100

    refine emulator script

--- ref_app/target/build/test_app_benchmarks_emulator.py
+++ ref_app/target/build/test_app_benchmarks_emulator.py
@@ -1,60 +1,97 @@
-﻿#!/usr/bin/env python2
+﻿#-------------------------------------------------------------------------------
+# Name:        test_app_benchmarks_emulator.py
+# Purpose:
+#
+# Author:      Christopher Kormanyos
+#
+# Created:     02/04/2021
+#
+# Copyright:   Copyright Christopher Kormanyos 2007 - 2022
+#
+# Licence:     Distributed under the Boost Software License,
+#              Version 1.0. (See accompanying file LICENSE_1_0.txt
+#              or copy at http://www.boost.org/LICENSE_1_0.txt)
+#-------------------------------------------------------------------------------
+
+#!/usr/bin/env python2
+
+# import python packages
 import gdb
 import time
 import logging
 import sys
 
-def execute(command, from_tty = False, to_string = False):
-    gdb.execute('{}'.format(command), from_tty, to_string)
+#-------------------------------------------------------------------------------
+# --- class: benchmarks_emulator
+#-------------------------------------------------------------------------------
+class benchmarks_emulator:
+    def __init__(self, tcp_port, iterations):
+        self.connect_to_server(tcp_port)
+        self.create_log_file()
+        self.load_elf()
 
-def connect_to_server(tcp_port):
-    execute('target remote localhost:{}'.format(tcp_port))
-    execute('monitor reset')
-    execute('set confirm off')
+    # Create log file
+    def create_log_file():
+        logging.basicConfig(filename='emu-target.log',level=logging.DEBUG, filemode='w')
+        logging.info('------- Running GDB Test -----')
 
-def load_elf():
-    execute('load')
+    def execute(command, from_tty = False, to_string = False):
+        gdb.execute('{}'.format(command), from_tty, to_string)
 
-def run():
-    execute('continue')
+    # Connect to server
+    def connect_to_server(tcp_port):
+        execute('target remote localhost:{}'.format(tcp_port))
+        execute('monitor reset')
+        execute('set confirm off')
 
-def next():
-    execute('next')
+    # Load object data base
+    def load_elf():
+        execute('load')
 
-def check_ret_val_and_quit_gdb(ret_val):
-    if ret_val == "0xf00dcafe":
-        sys.exit(0)
-    else:
-        sys.exit(-1)
+    # Run the benchmark
+    def run():
+        execute('continue')
+
+    def next():
+        execute('next')
+
+    def set_gdb_break_point():
+        return gdb.Breakpoint('app_benchmark_get_standalone_result')
+
+    def get_gdb_result():
+        return gdb.parse_and_eval("app_benchmark_standalone_result")
+
+    # Check the gdb return value
+    def check_ret_val_and_quit_gdb(ret_val):
+        val_as_str = str(ret_val)
+        val_as_hex = hex(int(val_as_str))
+
+        if val_as_hex == "0xf00dcafe":
+            sys.exit(0)
+        else:
+            sys.exit(-1)
 
 
-########################################################################################
-#Config
+#-------------------------------------------------------------------------------
+# --- GDB Script starts here
+# See also https://embeddedartistry.com/blog/2020/11/09/metal-gdb-controlling-gdb-through-python-scripts-with-the-gdb-python-api/
+#-------------------------------------------------------------------------------
+
+# Script Config
 tcp_port   = 9999
 iterations = 64
 
+# Initialize
+benchmarks_emulator(tcp_port, iterations)
 
-#GDB script
-#Connect to server
-connect_to_server(tcp_port)
+# Set break point and run the benchmark
+bp1 = benchmarks_emulator.set_gdb_break_point()
+benchmarks_emulator.run()
 
-#Create log file
-logging.basicConfig(filename='emu-target.log',level=logging.DEBUG, filemode='w')
-logging.info('------- Running GDB Test -----')
-
-#Load object data base
-load_elf()
-
-#See also https://embeddedartistry.com/blog/2020/11/09/metal-gdb-controlling-gdb-through-python-scripts-with-the-gdb-python-api/
-
-#Run the benchmark and verify the result
-bp1 = gdb.Breakpoint('app_benchmark_get_standalone_result')
-run()
-my_value = gdb.parse_and_eval("app_benchmark_standalone_result")
+# Get gdb result
+my_value = benchmarks_emulator.get_gdb_result()
 time.sleep(0.5)
 bp1.delete()
 
-# check the return value and quit gdb
-val_as_str = str(my_value)
-val_as_hex = hex(int(val_as_str))
-check_ret_val_and_quit_gdb(val_as_hex)
+# Check the return value and quit gdb
+check_ret_val_and_quit_gdb(my_value)
