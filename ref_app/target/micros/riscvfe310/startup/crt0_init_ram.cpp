625e942936e050022011e29bc9c939ca0d363aae
Christopher Kormanyos <ckormanyos@yahoo.com>
2022-12-23 12:36:03 +0100

    Disable CMake warnings directly on cmd line

--- ref_app/target/micros/riscvfe310/startup/crt0_init_ram.cpp
+++ ref_app/target/micros/riscvfe310/startup/crt0_init_ram.cpp
@@ -5,6 +5,7 @@
 //  or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
+#include <algorithm>
 #include <cstddef>
 #include <cstdint>
 
@@ -24,27 +25,21 @@ namespace crt
 
 void crt::init_ram()
 {
-  // Copy the data segment initializers from ROM to RAM.
-  const auto count_data =
-    static_cast<std::size_t>
-    (
-      (std::uintptr_t) &_data_end - (std::uintptr_t) &_data_begin
-    );
+  using memory_aligned_type = std::uint8_t;
 
-  for(auto i = static_cast<std::size_t>(UINT8_C(0)); i < count_data; ++i)
-  {
-    ((std::uint8_t*) &_data_begin)[i] = ((std::uint8_t*) &_rom_data_begin)[i];
-  }
+  // Copy the data segment initializers from ROM to RAM.
+  // Note that all data segments are aligned by 1.
+  const std::size_t size_data =
+    std::size_t(  static_cast<const memory_aligned_type*>(static_cast<const void*>(&_data_end))
+                - static_cast<const memory_aligned_type*>(static_cast<const void*>(&_data_begin)));
+
+  std::copy(static_cast<const memory_aligned_type*>(static_cast<const void*>(&_rom_data_begin)),
+            static_cast<const memory_aligned_type*>(static_cast<const void*>(&_rom_data_begin)) + size_data,
+            static_cast<      memory_aligned_type*>(static_cast<      void*>(&_data_begin)));
 
   // Clear the bss segment.
-  const auto count_bss =
-    static_cast<std::size_t>
-    (
-      (std::uintptr_t) &_bss_end - (std::uintptr_t) &_bss_begin
-    );
-
-  for(auto i = static_cast<std::size_t>(UINT8_C(0)); i < count_bss; ++i)
-  {
-    ((std::uint8_t*) &_bss_begin)[i] = static_cast<std::uint8_t>(UINT8_C(0));
-  }
+  // Note that the bss segment is aligned by 1.
+  std::fill(static_cast<memory_aligned_type*>(static_cast<void*>(&_bss_begin)),
+            static_cast<memory_aligned_type*>(static_cast<void*>(&_bss_end)),
+            static_cast<memory_aligned_type>(0U));
 }
