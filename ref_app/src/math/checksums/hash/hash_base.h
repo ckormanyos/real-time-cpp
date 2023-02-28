c6ee1c312f29fbcfb58bc089ccd4f3475cbd8256
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-02-28 09:15:19 +0100

    Tune hash algorithms

--- ref_app/src/math/checksums/hash/hash_base.h
+++ ref_app/src/math/checksums/hash/hash_base.h
@@ -15,18 +15,26 @@
   namespace math { namespace checksums { namespace hash {
 
   template<typename CountType,
-           const std::uint16_t MessageBufferSize>
+           const std::uint16_t ResultBitCount,
+           const std::uint16_t MessageBufferSize,
+           const std::uint16_t MessageLengthTotalBitCount>
   class hash_base
   {
   public:
     using count_type = CountType;
 
-    static_assert(   ( std::numeric_limits<count_type>::is_specialized == true)
-                  && ( std::numeric_limits<count_type>::is_integer     == true)
-                  && ( std::numeric_limits<count_type>::is_signed      == false)
-                  && ( std::numeric_limits<count_type>::radix          == 2)
-                  && ((std::numeric_limits<count_type>::digits % 8)    == 0),
-                  "Error: The count type must be an unsigned integer with radix 2, having a multiple of 8 bits");
+    using result_type = std::array<std::uint8_t, static_cast<std::size_t>(ResultBitCount / static_cast<std::uint16_t>(UINT8_C(8)))>;
+
+    static_assert
+    (
+           std::numeric_limits<count_type>::is_specialized
+      &&   std::numeric_limits<count_type>::is_integer
+      && (!std::numeric_limits<count_type>::is_signed)
+      && (std::numeric_limits<count_type>::radix == static_cast<int>(INT8_C(2)))
+      && (std::numeric_limits<count_type>::digits >= static_cast<int>(INT8_C(16)))
+      && (static_cast<int>(std::numeric_limits<count_type>::digits % static_cast<int>(INT8_C(8))) == static_cast<int>(INT8_C(0))),
+      "Error: The count type must be an unsigned integer with radix 2, that is 16 bits or wider, and having a multiple of 8 bits"
+    );
 
     virtual ~hash_base() = default;
 
@@ -34,6 +42,8 @@
     {
       message_index        = static_cast<std::uint_least16_t>(UINT8_C(0));
       message_length_total = static_cast<count_type>(UINT8_C(0));
+
+      message_buffer.fill(static_cast<std::uint8_t>(UINT8_C(0)));
     }
 
     auto process(const std::uint8_t* message, const count_type count) -> void
@@ -47,13 +57,13 @@
         message_length_total = static_cast<count_type>         (message_length_total + process_chunk_size);
         process_index        = static_cast<count_type>         (process_index        + process_chunk_size);
 
-        if(message_index == message_buffer_static_size)
+        if(message_index == message_buffer_static_size())
         {
-          this->perform_algorithm();
+          my_perform_algorithm();
         }
 
         process_chunk_size = (std::min)(static_cast<count_type>(count - process_index),
-                                        static_cast<count_type>(message_buffer_static_size - message_index));
+                                        static_cast<count_type>(message_buffer_static_size() - message_index));
 
         std::copy(message + static_cast<std::size_t>(process_index),
                   message + static_cast<std::size_t>(process_index + process_chunk_size),
@@ -64,65 +74,66 @@
 
     auto finalize() -> void
     {
-      message_block_type the_last_message_block;
-
-      std::copy(message_buffer.cbegin(),
-                message_buffer.cbegin() + message_index,
-                the_last_message_block.begin());
-
       // Create the padding. Begin by setting the leading padding byte to 0x80.
-      the_last_message_block[message_index] = static_cast<std::uint8_t>(UINT8_C(0x80));
+      message_buffer[message_index] = static_cast<std::uint8_t>(UINT8_C(0x80));
 
       ++message_index;
 
-      // Fill the rest of the padding bytes with zero.
-      std::fill(the_last_message_block.begin() + message_index,
-                the_last_message_block.end(),
-                static_cast<std::uint8_t>(UINT8_C(0)));
+      // We need an extra block because there are not enough bytes
+      // available for storing the total bit count. So we must
+      // transform the current block, then create and pad yet another
+      // additional block.
 
-      // Do we need an extra block? If so, then transform the
-      // current block and pad an additional block.
-      if(message_index > static_cast<std::uint16_t>(message_buffer_static_size - 8U))
+      if(static_cast<std::uint16_t>(message_index + static_cast<std::uint_least16_t>(message_length_total_width())) > message_buffer_static_size())
       {
-        message_buffer = the_last_message_block;
-
-        perform_algorithm();
-
-        the_last_message_block.fill(static_cast<std::uint8_t>(UINT8_C(0)));
+        my_perform_algorithm();
       }
 
-      // Encode the number of bits. Simultaneously convert the number of bytes
-      // to the number of bits by performing a left-shift of 3 on the byte-array.
-      // The hash_sha1 stores the 8 bytes of the bit counter in reverse order,
-      // with the lowest byte being stored at the highest position of the buffer
-      auto carry = static_cast<std::uint8_t>(UINT8_C(0));
+      // Encode the total number of bits in the final transform buffer.
+      // Here, we can easily see the conversion of the number of bytes
+      // to the number of bits. This is done by performing a left-shift
+      // of 3 on the variable "message_length_total".
 
-      auto local_message_length_total = static_cast<std::uint64_t>(message_length_total);
+      auto carry = static_cast<std::uint_fast8_t>(UINT8_C(0));
 
-      std::for_each(the_last_message_block.rbegin(),
-                    the_last_message_block.rbegin() + static_cast<std::size_t>(UINT8_C(8)),
-                    [&carry, &local_message_length_total](std::uint8_t& the_byte)
-                    {
-                      const std::uint_least16_t the_word =
-                        static_cast<std::uint_least16_t>(local_message_length_total << static_cast<unsigned>(UINT8_C(3)));
+      for(auto   ri  = message_buffer.rbegin();
+                 ri != message_buffer.rbegin() + message_length_total_width();
+               ++ri)
+      {
+        const std::uint_least16_t the_word =
+          static_cast<std::uint_least16_t>
+          (
+            static_cast<std::uint_least16_t>(message_length_total) << static_cast<unsigned>(UINT8_C(3))
+          );
 
-                      the_byte = static_cast<std::uint8_t>(the_word | carry);
+        *ri = static_cast<std::uint8_t>(the_word | carry);
 
-                      local_message_length_total >>= static_cast<unsigned>(UINT8_C(8));
+        message_length_total =
+          static_cast<count_type>
+          (
+            message_length_total >> static_cast<unsigned>(UINT8_C(8))
+          );
 
-                      carry =
-                        static_cast<std::uint8_t>
-                        (
-                            static_cast<std::uint8_t>(the_word >> static_cast<unsigned>(UINT8_C(8)))
-                          & static_cast<std::uint8_t>(UINT8_C(0x07))
-                        );
-                    });
+        carry =
+          static_cast<std::uint_fast8_t>
+          (
+              static_cast<std::uint_fast8_t>(the_word >> static_cast<unsigned>(UINT8_C(8)))
+            & static_cast<std::uint_fast8_t>(UINT8_C(0x07))
+          );
+      }
 
-      message_length_total = static_cast<count_type>(local_message_length_total);
+      my_perform_algorithm();
+    }
 
-      message_buffer = the_last_message_block;
-
-      this->perform_algorithm();
+    auto get_result(typename result_type::pointer result) -> void
+    {
+      // Extract the hash result from the message digest state.
+      detail::convert_uint32_input_to_uint8_output_reverse
+      (
+        transform_context.data(),
+        transform_context.data() + static_cast<std::size_t>(std::tuple_size<result_type>::value / sizeof(std::uint32_t)),
+        result
+      );
     }
 
     auto hash(const std::uint8_t* message, const count_type count) -> void
@@ -133,13 +144,19 @@
     }
 
   protected:
-    static constexpr auto message_buffer_static_size = MessageBufferSize;
+    using message_block_type = std::array<std::uint8_t, static_cast<std::size_t>(MessageBufferSize)>;
 
-    using message_block_type = std::array<std::uint8_t, static_cast<std::size_t>(message_buffer_static_size)>;
+    using context_type = std::array<std::uint32_t, static_cast<std::size_t>(std::tuple_size<result_type>::value / sizeof(std::uint32_t))>;
+
+    static constexpr auto message_buffer_static_size() -> std::uint16_t
+    {
+      return static_cast<std::uint16_t>(std::tuple_size<message_block_type>::value);
+    }
 
     std::uint_least16_t message_index        { };
     count_type          message_length_total { };
     message_block_type  message_buffer       { };
+    context_type        transform_context    { };
 
     hash_base() = default;
 
@@ -150,7 +167,25 @@
     auto operator=(hash_base&& other) noexcept -> hash_base& = default;
 
   private:
+    static constexpr auto message_length_total_width() noexcept -> std::uint16_t
+    {
+      return
+        static_cast<std::uint16_t>
+        (
+          MessageLengthTotalBitCount / static_cast<std::uint16_t>(UINT8_C(8))
+        );
+    }
+
     virtual auto perform_algorithm() -> void = 0;
+
+    auto my_perform_algorithm() -> void
+    {
+      this->perform_algorithm();
+
+      message_index = static_cast<std::uint_least16_t>(UINT8_C(0));
+
+      message_buffer.fill(static_cast<std::uint8_t>(UINT8_C(0)));
+    }
   };
 
   } } } // namespace math::checksums::hash
