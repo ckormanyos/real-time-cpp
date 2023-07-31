398edb5b8fc2d5b071a0418c293437d4460f0e70
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-07-31 16:16:35 +0200

    Retry chapter06 snippets in CI

--- code_snippets/chapter06/chapter06_09-003_alias_reg_access.cpp
+++ code_snippets/chapter06/chapter06_09-003_alias_reg_access.cpp
@@ -1,59 +1,83 @@
 ///////////////////////////////////////////////////////////////////////////////
-//  Copyright Christopher Kormanyos 2019.
+//  Copyright Christopher Kormanyos 2019 - 2023.
 //  Distributed under the Boost Software License,
 //  Version 1.0. (See accompanying file LICENSE_1_0.txt
 //  or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
-// chapter06_09-003_alias_reg_access.cpp
+// chapter06_09-001_typedef_reg_access.cpp
 
+#include <cstdint>
 #include <iomanip>
 #include <iostream>
-#include <cstdint>
 
-template<typename register_address_type,
-         typename register_value_type>
+template<typename RegisterAddressType,
+         typename RegisterValueType>
 struct reg_access_dynamic final
 {
-  static register_value_type
-              reg_get(const register_address_type address) { return *reinterpret_cast<volatile register_value_type*>(address); }
+  using register_address_type = RegisterAddressType;
+  using register_value_type   = RegisterValueType;
 
-  static void reg_set(const register_address_type address, const register_value_type value) { *reinterpret_cast<volatile register_value_type*>(address)  = value; }
-  static void reg_and(const register_address_type address, const register_value_type value) { *reinterpret_cast<volatile register_value_type*>(address) &= value; }
-  static void reg_or (const register_address_type address, const register_value_type value) { *reinterpret_cast<volatile register_value_type*>(address) |= value; }
-  static void reg_not(const register_address_type address, const register_value_type value) { *reinterpret_cast<volatile register_value_type*>(address) &= register_value_type(~value); }
-  static void reg_msk(const register_address_type address, const register_value_type value,
-                      const register_value_type mask_value)                                 { *reinterpret_cast<volatile register_value_type*>(address) = register_value_type(register_value_type(reg_get(address) & register_value_type(~mask_value)) | register_value_type(value & mask_value)); }
+  static auto reg_get(const register_address_type address) -> register_value_type                   { return *reinterpret_cast<volatile register_value_type*>(address); }
+  static auto reg_set(const register_address_type address, const register_value_type value) -> void { volatile register_value_type* pa = reinterpret_cast<volatile register_value_type*>(address); *pa = value; }
+  static auto reg_and(const register_address_type address, const register_value_type value) -> void { volatile register_value_type* pa = reinterpret_cast<volatile register_value_type*>(address); *pa = static_cast<register_value_type>(*pa & value); }
+  static auto reg_or (const register_address_type address, const register_value_type value) -> void { volatile register_value_type* pa = reinterpret_cast<volatile register_value_type*>(address); *pa = static_cast<register_value_type>(*pa | value); }
+  static auto reg_not(const register_address_type address, const register_value_type value) -> void { volatile register_value_type* pa = reinterpret_cast<volatile register_value_type*>(address); *pa = static_cast<register_value_type>(*pa & static_cast<register_value_type>(~value)); }
 
-  static void bit_set(const register_address_type address, const register_value_type value) { *reinterpret_cast<volatile register_value_type*>(address) |= static_cast<register_value_type>(1UL << value); }
-  static void bit_clr(const register_address_type address, const register_value_type value) { *reinterpret_cast<volatile register_value_type*>(address) &= static_cast<register_value_type>(~static_cast<register_value_type>(1UL << value)); }
-  static void bit_not(const register_address_type address, const register_value_type value) { *reinterpret_cast<volatile register_value_type*>(address) ^= static_cast<register_value_type>(1UL << value); }
-  static bool bit_get(const register_address_type address, const register_value_type value) { return (static_cast<volatile register_value_type>(reg_get(address) & static_cast<register_value_type>(1UL << value)) != static_cast<register_value_type>(0U)); }
+  static auto reg_msk(const register_address_type address,
+                      const register_value_type   value,
+                      const register_value_type   mask_value) -> void
+  {
+    volatile register_value_type* pa = reinterpret_cast<volatile register_value_type*>(address);
+
+    *pa =
+      static_cast<register_value_type>
+      (
+          static_cast<register_value_type>(reg_get(address) & static_cast<register_value_type>(~mask_value))
+        | value
+      );
+  }
+
+  static auto bit_set(const register_address_type address, const register_value_type value) -> void { volatile register_value_type* pa = reinterpret_cast<volatile register_value_type*>(address); *pa = static_cast<register_value_type>(*pa | static_cast<register_value_type>(1UL << value)); }
+  static auto bit_clr(const register_address_type address, const register_value_type value) -> void { volatile register_value_type* pa = reinterpret_cast<volatile register_value_type*>(address); *pa = static_cast<register_value_type>(*pa & static_cast<register_value_type>(~static_cast<register_value_type>(1UL << value))); }
+  static auto bit_not(const register_address_type address, const register_value_type value) -> void { volatile register_value_type* pa = reinterpret_cast<volatile register_value_type*>(address); *pa = static_cast<register_value_type>(*pa ^ static_cast<register_value_type>(1UL << value)); }
+  static auto bit_get(const register_address_type address, const register_value_type value) -> bool { return (static_cast<register_value_type>(reg_get(address) & static_cast<register_value_type>(1UL << value)) != static_cast<register_value_type>(0U)); }
 };
 
 // The simulated portb.
-std::uint8_t simulated_register_portb;
+auto simulated_register_portb = std::uint8_t { };
 
-const std::uintptr_t address =
-  reinterpret_cast<std::uintptr_t>(&simulated_register_portb);
+const auto address = reinterpret_cast<std::uintptr_t>(&simulated_register_portb);
 
-void do_something()
+auto do_something() -> void;
+
+auto do_something() -> void
 {
   // Use an alias to save tedious typing work.
   using port_type = reg_access_dynamic<std::uintptr_t, std::uint8_t>;
 
   // Toggle the simulated portb.5.
-  port_type::bit_not(address, 5u);
+  port_type::bit_not(address, static_cast<std::uint8_t>(UINT8_C(5)));
 }
 
-int main()
-{
-  do_something();
+auto main() -> int;
 
-  std::cout << "simulated_register_portb: "
-            << std::hex
-            << "0x"
-            << std::setw(2)
-            << unsigned(simulated_register_portb)
-            << std::endl;
+auto main() -> int
+{
+  const auto flg = std::cout.flags();
+
+  for(auto i = static_cast<unsigned>(UINT8_C(0)); i < static_cast<unsigned>(UINT8_C(4)); ++i)
+  {
+    do_something();
+
+    std::cout << "simulated_register_portb: "
+              << std::hex
+              << "0x"
+              << std::setw(static_cast<std::streamsize>(INT8_C(2)))
+              << std::setfill('0')
+              << static_cast<unsigned>(simulated_register_portb)
+              << std::endl;
+  }
+
+  std::cout.flags(flg);
 }
