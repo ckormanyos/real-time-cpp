d85981c9efbf895df94966bc6935abd356a6941f
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-07-31 14:29:13 +0200

    [ci skip] initial commit

--- code_snippets/chapter06/chapter06_14-003_romable_initializer_list.cpp
+++ code_snippets/chapter06/chapter06_14-003_romable_initializer_list.cpp
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////
-//  Copyright Christopher Kormanyos 2019.
+//  Copyright Christopher Kormanyos 2019 - 2023.
 //  Distributed under the Boost Software License,
 //  Version 1.0. (See accompanying file LICENSE_1_0.txt
 //  or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -7,33 +7,41 @@
 
 // chapter06_14-003_romable_initializer_list.cpp
 
+// See also https://godbolt.org/z/9Wejhs9xT
+
+#include <cstddef>
+#include <cstdint>
 #include <initializer_list>
 #include <iomanip>
 #include <iostream>
 
 namespace
 {
-  constexpr std::initializer_list<char>
-  version_string
-  {
-    { '1', '.', '2', '3', '\0' }
-  };
+  constexpr auto version_string =
+    std::initializer_list<char>
+    {
+      '1', '.', '2', '3', '\0'
+    };
 }
 
-static_assert(
-  *version_string.begin() == char('1'));
+static_assert(*version_string.begin() == '1',  "Error: Wrong value at begin of initializer_list.");
 
-int main()
+auto main() -> int;
+
+auto main() -> int
 {
+  const auto flg = std::cout.flags();
 
   std::cout << std::hex
             << "version_string[0U]: 0x"
             << std::setw(2)
-            << std::setfill(static_cast<char>('0'))
+            << std::setfill('0')
             << static_cast<unsigned>(*version_string.begin())
             << std::endl;
 
   std::cout << "version_string: "
             << version_string.begin()
             << std::endl;
+
+  std::cout.flags(flg);
 }
---
+++ code_snippets/chapter06/readme.md
@@ -0,0 +1,15 @@
+# Code Snippets
+## Chapter 6
+
+This folder contains the code snippets for Chapter 6.
+
+## Try it at _godbolt_
+
+<p align="center">
+    <a href="https://godbolt.org/z/9Wejhs9xT" alt="godbolt">
+        <img src="https://img.shields.io/badge/try%20it%20on-godbolt-green" /></a>
+</p>
+
+Try the [`chapter06_14-003_romable_initializer_list.cpp`](./chapter06_14-003_romable_initializer_list.cpp)
+program at this
+[short link](https://godbolt.org/z/9Wejhs9xT) to [godbolt](https://godbolt.org/).
