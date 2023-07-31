420a01d7497a0f87950bdf42344d534ba803de6a
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-07-31 10:09:07 +0200

    [ci skip] Add chapter04 docs

--- code_snippets/chapter04/chapter04_01-001_led_hierarchy.cpp
+++ code_snippets/chapter04/chapter04_01-001_led_hierarchy.cpp
@@ -1,11 +1,12 @@
 ///////////////////////////////////////////////////////////////////////////////
-//  Copyright Christopher Kormanyos 2018.
+//  Copyright Christopher Kormanyos 2018 - 2023.
 //  Distributed under the Boost Software License,
 //  Version 1.0. (See accompanying file LICENSE_1_0.txt
 //  or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
 // chapter04_01-001_led_hierarchy.cpp
+// See also https://godbolt.org/z/7MzKGEevh
 
 #include <algorithm>
 #include <cstdint>
@@ -14,42 +15,41 @@
 class led_base
 {
 public:
-  virtual void toggle() = 0; // Pure abstract.
-  virtual ~led_base() { }    // Virtual destructor.
+  virtual void toggle() = 0;     // Pure abstract.
+  virtual ~led_base() = default; // Virtual destructor.
 
   // Interface for querying the LED state.
-  bool state_is_on() const { return is_on; }
+  constexpr auto state_is_on() const noexcept -> bool { return is_on; }
+
+  // Non-implemented (deleted) copy constructors.
+  led_base(const led_base&) = delete;
+  led_base(led_base&&) noexcept = delete;
+
+  // Non-implemented (deleted) assignment operators.
+  constexpr auto operator=(const led_base&) -> led_base& = delete;
+  constexpr auto operator=(led_base&&) noexcept -> led_base& = delete;
 
 protected:
-  bool is_on;
+  bool is_on { };
 
   // A protected default constructor.
-  led_base() : is_on(false) { }
-
-private:
-  // Private non-implemented copy constructor.
-  led_base(const led_base&) = delete;
-
-  // Private non-implemented copy assignment operator.
-  const led_base& operator=(const led_base&) = delete;
+  constexpr led_base() = default;
 };
 
 class led_port : public led_base
 {
 public:
-  typedef std::uint8_t port_type;
-  typedef std::uint8_t bval_type;
+  using port_type = std::uint8_t;
+  using bval_type = std::uint8_t;
 
-  led_port(const port_type p,
-           const bval_type b) : port(p),
-                                bval(b)
-  {
-    // ...
-  }
+  explicit constexpr led_port(const port_type p,
+                              const bval_type b = static_cast<bval_type>(UINT8_C(0)))
+    : port(p),
+      bval(b) { }
 
-  virtual ~led_port() { }
+  ~led_port() override = default;
 
-  virtual void toggle()
+  auto toggle() -> void override
   {
     // Toggle the is_on indication flag.
     is_on = (!is_on);
@@ -68,28 +68,26 @@ private:
 class pwm final // A placeholder pwm class used for exemplary purposes.
 {
 public:
-  explicit pwm(const std::uint8_t c)
+  explicit constexpr pwm(const std::uint8_t c)
     : channel   (c),
-      duty_cycle(0U) { }
+      duty_cycle(static_cast<std::uint8_t>(UINT8_C(0))) { }
 
-  ~pwm() = default;
-
-  void set_duty(const std::uint8_t duty)
+  constexpr auto set_duty(const std::uint8_t duty) -> void
   {
     // Limit the duty cycle to 0...100.
-    duty_cycle = (std::min)(std::uint8_t(duty),
-                            std::uint8_t(100U));
+    duty_cycle = (std::min)(static_cast<std::uint8_t>(duty),
+                            static_cast<std::uint8_t>(UINT8_C(100)));
 
     // Set the duty cycle in the PWM hardware.
     // ...
   }
 
-  std::uint8_t get_duty() const
+  constexpr auto get_duty() const -> std::uint8_t
   {
     return duty_cycle;
   }
 
-  std::uint8_t get_channel() const { return channel; }
+  constexpr auto get_channel() const noexcept -> std::uint8_t { return channel; }
 
 private:
   std::uint8_t channel;
@@ -101,53 +99,67 @@ class led_pwm : public led_base
 public:
   explicit led_pwm(pwm* p) : my_pwm(p) { }
 
-  virtual ~led_pwm() = default;
+  ~led_pwm() override = default;
 
-  virtual void toggle()
+  auto toggle() -> void override
   {
     // Toggle the duty cycle.
-    const  std::uint8_t duty =
-      (my_pwm->get_duty() > 0U) ? 0U : 100U;
+    const auto duty =
+      static_cast<std::uint8_t>
+      (
+        (my_pwm->get_duty() > static_cast<std::uint8_t>(UINT8_C(0)))
+          ? static_cast<std::uint8_t>(UINT8_C(0))
+          : static_cast<std::uint8_t>(UINT8_C(100))
+      );
 
     my_pwm->set_duty(duty);
 
     // Toggle the LED with the PWM signal.
-    is_on = (my_pwm->get_duty() > 0U);
+    is_on = (my_pwm->get_duty() > static_cast<std::uint8_t>(UINT8_C(0)));
 
     // Show the PC simulated pwm LED.
-    std::cout << "LED pwm(" << std::dec << unsigned(my_pwm->get_channel()) << "): ";
+    std::cout << "LED pwm(" << std::dec << static_cast<unsigned>(my_pwm->get_channel()) << "): ";
 
     std::cout << (is_on ? "is on" : "is off") << std::endl;
   }
 
   // This LED class also supports dimming.
-  void dimming(const std::uint8_t duty)
+  auto dimming(const std::uint8_t duty) -> void
   {
     my_pwm->set_duty(duty);
 
-    is_on = (duty != 0U);
+    is_on = (duty != static_cast<std::uint8_t>(UINT8_C(0)));
   }
 
 private:
-  pwm* my_pwm;
+  pwm* my_pwm { nullptr };
 };
 
 namespace
 {
   // Two simulated port LEDs.
-  led_port led0 { 0x12, 1U };
-  led_port led1 { 0x34, 2U };
+  led_port led0
+  {
+    static_cast<led_port::port_type>(UINT8_C(0x12)),
+    static_cast<led_port::bval_type>(UINT8_C(1))
+  };
+
+  led_port led1
+  {
+    static_cast<led_port::port_type>(UINT8_C(0x34)),
+    static_cast<led_port::bval_type>(UINT8_C(2))
+  };
 
   // Two simulated PWMs.
-  pwm pwm0 { 0U };
-  pwm pwm1 { 1U };
+  pwm pwm0 { static_cast<std::uint8_t>(UINT8_C(0)) };
+  pwm pwm1 { static_cast<std::uint8_t>(UINT8_C(1)) };
 
   // Two simulated pwm LEDs.
-  led_pwm led2 { &pwm0 };
-  led_pwm led3 { &pwm1 };
+  led_pwm led2(&pwm0);
+  led_pwm led3(&pwm1);
 }
 
-void do_something()
+auto do_something() -> void
 {
   // Toggle led0-led3.
   led0.toggle();
@@ -156,9 +168,14 @@ void do_something()
   led3.toggle();
 }
 
-int main()
+auto main() -> int;
+
+auto main() -> int
 {
-  for(;;)
+  // Toggle all 4 LEDs in a loop 4 times times.
+  // (The version(s) for the embedded systems toggle in a forever-loop.)
+
+  for(auto i = static_cast<unsigned>(UINT8_C(0)); i < static_cast<unsigned>(UINT8_C(4)); ++i)
   {
     do_something();
   }
