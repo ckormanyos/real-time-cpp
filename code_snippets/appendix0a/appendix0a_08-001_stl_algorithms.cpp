1a4cbb7195f822fdf1d1de39c36f4432f99f090e
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-08-01 11:54:54 +0200

    Correct a programming warning/error

--- code_snippets/appendix0a/appendix0a_08-001_stl_algorithms.cpp
+++ code_snippets/appendix0a/appendix0a_08-001_stl_algorithms.cpp
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////
-//  Copyright Christopher Kormanyos 2019.
+//  Copyright Christopher Kormanyos 2019 - 2023.
 //  Distributed under the Boost Software License,
 //  Version 1.0. (See accompanying file LICENSE_1_0.txt
 //  or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -9,7 +9,7 @@
 
 #include <iostream>
 
-namespace std
+namespace non_std
 {
   // Declaration of template function std::for_each().
   template<typename iterator_type,
@@ -22,9 +22,9 @@ namespace std
 // Implementation of template function std::for_each().
 template<typename iterator_type,
          typename function_type>
-function_type std::for_each(iterator_type first,
-                            iterator_type last,
-                            function_type function)
+function_type non_std::for_each(iterator_type first,
+                                iterator_type last,
+                                function_type function)
 {
   while(first != last)
   {
@@ -36,19 +36,23 @@ function_type std::for_each(iterator_type first,
   return function;
 }
 
-int main()
+auto main() -> int;
+
+auto main() -> int
 {
   const int n3[] = { 1, 2, 3 };
+
+  using non_std::for_each;
 
   // Print each element of n3 using for_each
   // in combination with a lambda function.
   // Lambda functions are described in Sect. A9.
-  std::for_each(n3,
-                n3 + 3U,
-                [](const int& n)
-                {
-                  std::cout << n << ",";
-                });
+  for_each(n3,
+           n3 + 3U,
+           [](const int& n)
+           {
+             std::cout << n << ",";
+           });
 
   std::cout << std::endl;
 }
