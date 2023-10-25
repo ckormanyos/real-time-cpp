8c48d2a7591ff4f6507c850f60ff598f479f0783
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-10-25 08:06:55 +0200

    Eliminate snippet 09_03-001 from CI for clang++

--- code_snippets/chapter09/chapter09.sh
+++ code_snippets/chapter09/chapter09.sh
@@ -24,16 +24,26 @@ rm -f ./bin/*.*
 
 echo build snippets with GCC=$GCC STD=$STD
 
-$GCC -std=$STD     -Wall -Wextra -Wpedantic -Werror -Wconversion -Wsign-conversion -O3 -m64 -fsanitize=address -fsanitize=shift -fsanitize=shift-exponent -fsanitize=shift-base -fsanitize=integer-divide-by-zero -fsanitize=null -fsanitize=signed-integer-overflow -fsanitize=bounds -fsanitize=alignment -fsanitize=float-divide-by-zero -fsanitize=float-cast-overflow -fsanitize=enum ./chapter09_01-001_port_pin_template.cpp         -o ./bin/chapter09_01-001_port_pin_template.exe
-$GCC -std=$STD     -Wall -Wextra -Wpedantic -Werror -Wconversion -Wsign-conversion -O3 -m64 -fsanitize=address -fsanitize=shift -fsanitize=shift-exponent -fsanitize=shift-base -fsanitize=integer-divide-by-zero -fsanitize=null -fsanitize=signed-integer-overflow -fsanitize=bounds -fsanitize=alignment -fsanitize=float-divide-by-zero -fsanitize=float-cast-overflow -fsanitize=enum ./chapter09_02-001_programming_interrupts.cpp    -o ./bin/chapter09_02-001_programming_interrupts.exe
-$GCC -std=$STD     -Wall -Wextra -Wpedantic -Werror -Wconversion -Wsign-conversion -O3 -m64 -fsanitize=address -fsanitize=shift -fsanitize=shift-exponent -fsanitize=shift-base -fsanitize=integer-divide-by-zero -fsanitize=null -fsanitize=signed-integer-overflow -fsanitize=bounds -fsanitize=alignment -fsanitize=float-divide-by-zero -fsanitize=float-cast-overflow -fsanitize=enum ./chapter09_02-002_enable_timer_interrupt.cpp    -o ./bin/chapter09_02-002_enable_timer_interrupt.exe
-$GCC -std=$STD     -Wall -Wextra -Wpedantic -Werror -Wconversion -Wsign-conversion -O3 -m64 -fsanitize=address -fsanitize=shift -fsanitize=shift-exponent -fsanitize=shift-base -fsanitize=integer-divide-by-zero -fsanitize=null -fsanitize=signed-integer-overflow -fsanitize=bounds -fsanitize=alignment -fsanitize=float-divide-by-zero -fsanitize=float-cast-overflow -fsanitize=enum ./chapter09_03-001_system_tick.cpp               -o ./bin/chapter09_03-001_system_tick.exe
 
+$GCC -std=$STD -Wall -Wextra -Wpedantic -Werror -Wconversion -Wsign-conversion -O3 -m64 -fsanitize=address -fsanitize=shift -fsanitize=shift-exponent -fsanitize=shift-base -fsanitize=integer-divide-by-zero -fsanitize=null -fsanitize=signed-integer-overflow -fsanitize=bounds -fsanitize=alignment -fsanitize=float-divide-by-zero -fsanitize=float-cast-overflow -fsanitize=enum ./chapter09_01-001_port_pin_template.cpp         -o ./bin/chapter09_01-001_port_pin_template.exe
+$GCC -std=$STD -Wall -Wextra -Wpedantic -Werror -Wconversion -Wsign-conversion -O3 -m64 -fsanitize=address -fsanitize=shift -fsanitize=shift-exponent -fsanitize=shift-base -fsanitize=integer-divide-by-zero -fsanitize=null -fsanitize=signed-integer-overflow -fsanitize=bounds -fsanitize=alignment -fsanitize=float-divide-by-zero -fsanitize=float-cast-overflow -fsanitize=enum ./chapter09_02-001_programming_interrupts.cpp    -o ./bin/chapter09_02-001_programming_interrupts.exe
+$GCC -std=$STD -Wall -Wextra -Wpedantic -Werror -Wconversion -Wsign-conversion -O3 -m64 -fsanitize=address -fsanitize=shift -fsanitize=shift-exponent -fsanitize=shift-base -fsanitize=integer-divide-by-zero -fsanitize=null -fsanitize=signed-integer-overflow -fsanitize=bounds -fsanitize=alignment -fsanitize=float-divide-by-zero -fsanitize=float-cast-overflow -fsanitize=enum ./chapter09_02-002_enable_timer_interrupt.cpp    -o ./bin/chapter09_02-002_enable_timer_interrupt.exe
+if [[ "$GCC" == "g++" ]]; then
+$GCC -std=$STD -Wall -Wextra -Wpedantic -Werror -Wconversion -Wsign-conversion -O3 -m64 -fsanitize=address -fsanitize=shift -fsanitize=shift-exponent -fsanitize=shift-base -fsanitize=integer-divide-by-zero -fsanitize=null -fsanitize=signed-integer-overflow -fsanitize=bounds -fsanitize=alignment -fsanitize=float-divide-by-zero -fsanitize=float-cast-overflow -fsanitize=enum ./chapter09_03-001_system_tick.cpp               -o ./bin/chapter09_03-001_system_tick.exe
+fi
+
+if [[ "$GCC" == "g++" ]]; then
 ls -la \
 ./bin/chapter09_01-001_port_pin_template.exe              \
 ./bin/chapter09_02-001_programming_interrupts.exe         \
 ./bin/chapter09_02-002_enable_timer_interrupt.exe         \
 ./bin/chapter09_03-001_system_tick.exe
+else
+ls -la \
+./bin/chapter09_01-001_port_pin_template.exe              \
+./bin/chapter09_02-001_programming_interrupts.exe         \
+./bin/chapter09_02-002_enable_timer_interrupt.exe
+fi
 
 result_ls=$?
 
