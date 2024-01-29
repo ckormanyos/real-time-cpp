675e1f2c01311c3c62f36d9eb0eb21cde4830a2c
Christopher Kormanyos <ckormanyos@yahoo.com>
2024-01-29 04:09:50 +0100

    Only disable C-LIB for embedded

--- ref_app/src/app/benchmark/app_benchmark_non_std_decimal.cpp
+++ ref_app/src/app/benchmark/app_benchmark_non_std_decimal.cpp
@@ -13,8 +13,10 @@
 
 #if(APP_BENCHMARK_TYPE == APP_BENCHMARK_TYPE_NON_STD_DECIMAL)
 
+#if (defined(__arm__) || defined(__AVR__))
 #if !defined(BOOST_DECIMAL_DISABLE_CLIB)
 #define BOOST_DECIMAL_DISABLE_CLIB
+#endif
 #endif
 
 #include <boost/decimal.hpp>
