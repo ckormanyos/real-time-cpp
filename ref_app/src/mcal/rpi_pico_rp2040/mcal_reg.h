18c7ab6f3d76e2fdcb1e5b30d541dd964bc5049a
ckormanyos <ckormanyos@yahoo.com>
2024-07-27 19:58:56 +0200

    Refactor osc-init and remove C-style clocks

--- ref_app/src/mcal/rpi_pico_rp2040/mcal_reg.h
+++ ref_app/src/mcal/rpi_pico_rp2040/mcal_reg.h
@@ -15,8 +15,8 @@
     namespace reg
     {
       // Global base addresses.
-      constexpr std::uint32_t scs_base        = UINT32_C(0xE000E000);
-      constexpr std::uint32_t nvic_base       = scs_base + UINT32_C(0x00000100);
+      constexpr std::uint32_t scs_base                  { UINT32_C(0xE000E000) };
+      constexpr std::uint32_t nvic_base                 { scs_base + UINT32_C(0x00000100) };
 
       // Interrupt control registers.
       constexpr std::uint32_t nvic_iser { nvic_base + UINT32_C(0x00000000) };  // Offset: 0x000 Interrupt set enable registers.
@@ -24,6 +24,33 @@
       constexpr std::uint32_t nvic_ispr { nvic_base + UINT32_C(0x00000100) };  // Offset: 0x100 Interrupt set pending registers.
       constexpr std::uint32_t nvic_icpr { nvic_base + UINT32_C(0x00000180) };  // Offset: 0x180 Interrupt clear pending registers.
       constexpr std::uint32_t nvic_ip   { nvic_base + UINT32_C(0x00000300) };  // Offset: 0x300 Interrupt priority registers (each one 8 bits wide).
+
+      constexpr std::uint32_t xosc_base                 { UINT32_C(0x40024000) };
+      constexpr std::uint32_t xosc_ctrl                 { xosc_base + UINT32_C(0x00000000) };
+      constexpr std::uint32_t xosc_status               { xosc_base + UINT32_C(0x00000004) };
+      constexpr std::uint32_t xosc_dormant              { xosc_base + UINT32_C(0x00000008) };
+      constexpr std::uint32_t xosc_startup              { xosc_base + UINT32_C(0x0000000C) };
+      constexpr std::uint32_t xosc_reserved_0           { xosc_base + UINT32_C(0x00000010) };
+      constexpr std::uint32_t xosc_reserved_1           { xosc_base + UINT32_C(0x00000014) };
+      constexpr std::uint32_t xosc_reserved_2           { xosc_base + UINT32_C(0x00000018) };
+      constexpr std::uint32_t xosc_count                { xosc_base + UINT32_C(0x0000001C) };
+
+      constexpr std::uint32_t resets_base               { UINT32_C(0x4000C000) };
+      constexpr std::uint32_t resets_reset              { resets_base + UINT32_C(0x00000000) };
+      constexpr std::uint32_t resets_wdsel              { resets_base + UINT32_C(0x00000004) };
+      constexpr std::uint32_t resets_reset_done         { resets_base + UINT32_C(0x00000008) };
+
+      constexpr std::uint32_t pll_sys_base              { UINT32_C(0x40028000) };
+      constexpr std::uint32_t pll_sys_cs                { pll_sys_base + UINT32_C(0x00000000) };
+      constexpr std::uint32_t pll_sys_pwr               { pll_sys_base + UINT32_C(0x00000004) };
+      constexpr std::uint32_t pll_sys_fbdiv_int         { pll_sys_base + UINT32_C(0x00000008) };
+      constexpr std::uint32_t pll_sys_prim              { pll_sys_base + UINT32_C(0x0000000C) };
+
+      constexpr std::uint32_t clocks_base               { UINT32_C(0x40008000) };
+      constexpr std::uint32_t clocks_clk_sys_ctrl       { clocks_base + UINT32_C(0x0000003C) };
+      constexpr std::uint32_t clocks_clk_sys_div        { clocks_base + UINT32_C(0x00000040) };
+      constexpr std::uint32_t clocks_clk_sys_selected   { clocks_base + UINT32_C(0x00000044) };
+      constexpr std::uint32_t clocks_clk_peri_ctrl      { clocks_base + UINT32_C(0x00000048) };
     }
   }
 
