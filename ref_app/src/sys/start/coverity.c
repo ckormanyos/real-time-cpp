4cdb1af54746aa2154adb3b41ddd42a33f2595c4
ckormanyos <ckormanyos@yahoo.com>
2024-09-30 09:45:54 +0200

    Some syntax and upgrade coverity

--- ref_app/src/sys/start/coverity.c
+++ ref_app/src/sys/start/coverity.c
@@ -1,5 +1,8 @@
 /* Coverity Scan model */
 
+/* cd /mnt/c/Users/ckorm/Documents/Ks/uC_Software/Boards/real-time-cpp/ref_app */
+/* target/build/build.sh x86_64-w64-mingw32 rebuild */
+
 void dummy(void);
 
 void dummy(void)
