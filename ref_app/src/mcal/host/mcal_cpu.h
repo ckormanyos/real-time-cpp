820f9358ce66ed6f9123bbcb6fdfbc998e294d58
ckormanyos <ckormanyos@yahoo.com>
2024-10-11 13:34:45 +0200

    Continue addressing CI issues

--- ref_app/src/mcal/host/mcal_cpu.h
+++ ref_app/src/mcal/host/mcal_cpu.h
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////
-//  Copyright Christopher Kormanyos 2007 - 2023.
+//  Copyright Christopher Kormanyos 2007 - 2024.
 //  Distributed under the Boost Software License,
 //  Version 1.0. (See accompanying file LICENSE_1_0.txt
 //  or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -24,7 +24,7 @@
 
       inline auto nop() -> void { }
 
-    } // namespace mcal
-  } // namespace cpu
+    } // namespace cpu
+  } // namespace mcal
 
 #endif // MCAL_CPU_2009_02_14_H
--- ref_app/src/sys/start/coverity.c
+++ ref_app/src/sys/start/coverity.c
@@ -3,6 +3,7 @@
 /* cd /mnt/c/Users/ckorm/Documents/Ks/uC_Software/Boards/real-time-cpp/ref_app */
 /* PATH=/home/chris/coverity/cov-analysis-linux64-2023.12.2/bin:$PATH */
 /* cov-build --dir cov-int target/build/build.sh x86_64-w64-mingw32 rebuild */
+/* tar caf real-time-cpp.bz2 cov-int */
 
 void dummy(void);
 
