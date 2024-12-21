7110a740ef355afa829adfaad87684a703099a3b
ckormanyos <ckormanyos@yahoo.com>
2024-12-21 10:14:49 +0100

    Add modern avrdude and batches

---
+++ ref_app/tools/AVR/avrdude/avrdude-v8.0-windows-x64/2560.bat
@@ -0,0 +1,17 @@
+echo off
+
+set AVRDUDE=.\avrdude.exe
+
+set HEX=../../../../bin/ref_app.hex
+
+rem Erase the chip.
+echo "Erase the chip."
+%AVRDUDE% -c avrisp2 -p atmega2560 -P usb -e
+
+rem Flash the HEX-file.
+echo "Flash the HEX-file."
+%AVRDUDE% -c avrisp2 -p atmega2560 -P usb -U flash:w:%HEX%:i
+
+rem Verify the flash.
+echo "Verify the flash."
+%AVRDUDE% -c avrisp2 -p atmega2560 -P usb -U flash:v:%HEX%:i
