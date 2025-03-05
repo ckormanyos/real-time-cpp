4fc8b7abef5cee6e272d45464b221430bcfa3cf9
ckormanyos <ckormanyos@yahoo.com>
2025-03-05 13:45:18 +0100

    Add more FP patches and do cleanup

--- ref_app/target/micros/xtensa_esp32_s3/startup/from_no_sdk/Std/StdLib.cpp
+++ ref_app/target/micros/xtensa_esp32_s3/startup/from_no_sdk/Std/StdLib.cpp
@@ -27,29 +27,25 @@
 #include <cstdint>
 #include <cstring>
 
-extern "C"
-{
-  typedef   signed long long DItype  __attribute__((mode (DI)));
-  typedef unsigned long long UDItype __attribute__((mode (DI)));
-  typedef unsigned int       USItype __attribute__((mode (SI)));
+extern "C" {
 
-  extern int __builtin_clzll(long long unsigned int);
-}
+typedef   signed long long DItype  __attribute__((mode (DI)));
+typedef unsigned long long UDItype __attribute__((mode (DI)));
+typedef unsigned int       USItype __attribute__((mode (SI)));
+
+extern int __builtin_clzll(long long unsigned int);
 
 #define DWtype  DItype
 #define UDWtype UDItype
 #define UWtype  USItype
 
-extern "C"
 UDWtype __udivmoddi4 (UDWtype n, UDWtype d, UDWtype *rp);
 
-extern "C"
 UDWtype __udivdi3 (UDWtype n, UDWtype d)
 {
   return __udivmoddi4 (n, d, (UDWtype *) 0);
 }
 
-extern "C"
 UDWtype __umoddi3 (UDWtype u, UDWtype v)
 {
   UDWtype w;
@@ -59,19 +55,18 @@ UDWtype __umoddi3 (UDWtype u, UDWtype v)
   return w;
 }
 
-extern "C"
 UDWtype __udivmoddi4 (UDWtype n, UDWtype d, UDWtype *rp)
 {
   UDWtype q = 0, r = n, y = d;
   UWtype lz1, lz2, i, k;
 
-  /* Implements align divisor shift dividend method. This algorithm
-     aligns the divisor under the dividend and then perform number of
-     test-subtract iterations which shift the dividend left. Number of
-     iterations is k + 1 where k is the number of bit positions the
-     divisor must be shifted left to align it under the dividend.
-     quotient bits can be saved in the rightmost positions of the dividend
-     as it shifts left on each test-subtract iteration. */
+  // Implements align divisor shift dividend method. This algorithm
+  // aligns the divisor under the dividend and then perform number of
+  // test-subtract iterations which shift the dividend left. Number of
+  // iterations is k + 1 where k is the number of bit positions the
+  // divisor must be shifted left to align it under the dividend.
+  // quotient bits can be saved in the rightmost positions of the dividend
+  // as it shifts left on each test-subtract iteration.
 
   if (y <= r)
   {
@@ -81,11 +76,12 @@ UDWtype __udivmoddi4 (UDWtype n, UDWtype d, UDWtype *rp)
     k = lz1 - lz2;
     y = (y << k);
 
-       /* Dividend can exceed 2 ^ (width - 1) - 1 but still be less than the
-    aligned divisor. Normal iteration can drops the high order bit
-    of the dividend. Therefore, first test-subtract iteration is a
-    special case, saving its quotient bit in a separate location and
-    not shifting the dividend. */
+    // Dividend can exceed 2 ^ (width - 1) - 1 but still be less than the
+    // aligned divisor. Normal iteration can drops the high order bit
+    // of the dividend. Therefore, first test-subtract iteration is a
+    // special case, saving its quotient bit in a separate location and
+    // not shifting the dividend.
+
     if (r >= y)
     {
       r = r - y;
@@ -95,24 +91,30 @@ UDWtype __udivmoddi4 (UDWtype n, UDWtype d, UDWtype *rp)
     if (k > 0)
     {
       y = y >> 1;
-    
-      /* k additional iterations where k regular test subtract shift
-        dividend iterations are done.  */
+
+      // k additional iterations where k regular test subtract shift
+      // dividend iterations are done.
+
       i = k;
-    
+
       do
       {
         if (r >= y)
+        {
           r = ((r - y) << 1) + 1;
+        }
         else
+        {
           r =  (r << 1);
-    
+        }
+
         i = i - 1;
       }
       while (i != 0);
-    
-      /* First quotient bit is combined with the quotient bits resulting
-         from the k regular iterations.  */
+
+      // First quotient bit is combined with the quotient bits resulting
+      // from the k regular iterations.
+
       q = q + r;
       r = r >> k;
       q = q - (r << k);
@@ -120,31 +122,23 @@ UDWtype __udivmoddi4 (UDWtype n, UDWtype d, UDWtype *rp)
   }
 
   if (rp)
+  {
     *rp = r;
+  }
 
   return q;
 }
 
-extern "C"
-float __ieee754_sqrtf(float);
-
-extern "C"
-float sqrtf(float x)
-{
-  return __ieee754_sqrtf(x);
-}
-
-extern "C"
 void* memset(void* str, int c, size_t n)
 {
   uint8_t *ptr = (uint8_t*) str;
   uint32_t value = (uint8_t) c;
 
-  // Set value to repeat the byte across a 32-bit word
+  // Set value to repeat the byte across a 32-bit word.
   value |= value << 8;
   value |= value << 16;
 
-  // Align to the next 32-bit boundary
+  // Align to the next 32-bit boundary.
   while (((uintptr_t)ptr & 3) && n > 0)
   {
     *ptr++ = (uint8_t) c;
@@ -152,7 +146,7 @@ void* memset(void* str, int c, size_t n)
     --n;
   }
 
-  // Set memory in 32-bit chunks
+  // Set memory in 32-bit chunks.
   uint32_t* ptr32 = (uint32_t*) ptr;
 
   while (n >= 4)
@@ -162,7 +156,7 @@ void* memset(void* str, int c, size_t n)
     n -= 4;
   }
 
-  // Handle any remaining bytes
+  // Handle any remaining bytes.
   ptr = (uint8_t*) ptr32;
 
   while (n > 0)
@@ -175,13 +169,12 @@ void* memset(void* str, int c, size_t n)
   return str;
 }
 
-extern "C"
 void* memcpy (void* dest, const void* src, size_t n)
 {
   uint8_t *d = (uint8_t*) dest;
   const uint8_t* s = (const uint8_t*) src;
 
-  // Align destination to the next 32-bit boundary
+  // Align destination to the next 32-bit boundary.
   while (((uintptr_t) d & 3) && n > 0)
   {
     *d++ = *s++;
@@ -189,7 +182,8 @@ void* memcpy (void* dest, const void* src, size_t n)
     --n;
   }
 
-  // Copy memory in 32-bit chunks
+  // Copy memory in 32-bit chunks.
+
   uint32_t *d32 = (uint32_t*) d;
   const uint32_t* s32 = (const uint32_t*) s;
 
@@ -200,7 +194,8 @@ void* memcpy (void* dest, const void* src, size_t n)
     n -= 4;
   }
 
-  // Handle any remaining bytes
+  // Handle any remaining bytes.
+
   d = (uint8_t*) d32;
   s = (const uint8_t*) s32;
 
@@ -214,7 +209,6 @@ void* memcpy (void* dest, const void* src, size_t n)
   return dest;
 }
 
-extern "C"
 void* memmove (void* dest, const void* src, size_t n)
 {
   uint8_t* from = (uint8_t*) src;
@@ -231,6 +225,7 @@ void* memmove (void* dest, const void* src, size_t n)
     //  <from......>
     //         <to........>
     // copy in reverse, to avoid overwriting from
+
     for(ptrdiff_t i = (ptrdiff_t) (n - 1); i >= 0; --i)
     {
       to[i] = from[i];
@@ -244,7 +239,8 @@ void* memmove (void* dest, const void* src, size_t n)
     // to overlaps with from
     //        <from......>
     //  <to........>
-    // copy forwards, to avoid overwriting from */
+    // copy forwards, to avoid overwriting from
+
     for(size_t i = 0U; i < n; ++i)
     {
       to[i] = from[i];
@@ -257,3 +253,5 @@ void* memmove (void* dest, const void* src, size_t n)
 
   return dest;
 }
+
+} // extern "C"
