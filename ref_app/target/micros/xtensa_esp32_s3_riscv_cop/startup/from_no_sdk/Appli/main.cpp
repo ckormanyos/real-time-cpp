f7002b01683cf49ebbe38e1186b6cc1ad79c7de2
ckormanyos <ckormanyos@yahoo.com>
2025-03-10 08:58:50 +0100

    esp32s3 cleanup and add cop main

---
+++ ref_app/target/micros/xtensa_esp32_s3_riscv_cop/startup/from_no_sdk/Appli/main.cpp
@@ -0,0 +1,25 @@
+/******************************************************************************************
+  Filename    : main.c
+  
+  Core        : RISC-V
+  
+  MCU         : ESP32-S3
+    
+  Author      : Chalandi Amine
+ 
+  Owner       : Chalandi Amine
+  
+  Date        : 22.02.2025
+  
+  Description : Application main function for ULP-RISC-V Co-processor
+  
+******************************************************************************************/
+
+int main()
+{
+  for(;;)
+  {
+    ;
+  }
+}
+
