d74a1d4e5133878b03ba7edd3f6ef238299f8fa1
ckormanyos <ckormanyos@yahoo.com>
2025-08-01 14:51:22 +0200

    Repair the snippet for 3.21 as in issue658

--- code_snippets/chapter03/chapter03_21-001_span.cpp
+++ code_snippets/chapter03/chapter03_21-001_span.cpp
@@ -1,24 +1,30 @@
 /////////////////////////////////////////////////////////////////
-//  Copyright Christopher Kormanyos 2018 - 2023.
+//  Copyright Christopher Kormanyos 2018 - 2025.
 //  Distributed under the Boost Software License,
 //  Version 1.0. (See accompanying file LICENSE_1_0.txt
 //  or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
+// Repaired in: https://github.com/ckormanyos/real-time-cpp/issues/658
+
 // chapter03_21-001_span.cpp
 
+#include <algorithm>
 #include <cstdint>
+#include <iomanip>
 #include <iostream>
 #include <iterator>
 #include <span>
+#include <sstream>
 
-void clear_buffer(std::span<std::uint8_t> pb)
+namespace
 {
-  for(auto u : pb)
+  void clear_buffer(std::span<std::uint8_t> buf)
   {
-    u = static_cast<std::uint8_t>(UINT8_C(0));
-
-    static_cast<void>(u);
+    for(auto& next_byte : buf)
+    {
+      next_byte = static_cast<std::uint8_t>(UINT8_C(0));
+    }
   }
 }
 
@@ -33,6 +39,19 @@ void do_something()
 
 int main()
 {
-  // 0
-  std::cout << unsigned(*(std::cend(legacy_buffer) - 1U)) << std::endl;
+  std::span<std::uint8_t> buffer_span(legacy_buffer);
+
+  // First off, fill the legacy buffer with a pattern.
+  std::fill(buffer_span.begin(), buffer_span.end(), UINT8_C(0x5A));
+
+  std::stringstream strm { };
+
+  // Ensure that the buffer has the pattern.
+  strm << "filled:  " << std::hex << std::uppercase << std::setw(std::streamsize { 2 }) << std::setfill('0') << unsigned(buffer_span.back());
+
+  // Now clear the buffer and the buffer has been cleared to 0.
+  do_something(buffer_span);
+  strm << "\ncleared: " << std::hex << std::uppercase << std::setw(std::streamsize { 2 }) << std::setfill('0') << unsigned(buffer_span.back());
+
+  std::cout << strm.str() << std::endl;
 }
