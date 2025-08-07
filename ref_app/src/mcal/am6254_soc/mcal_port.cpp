2f3bf0b2f9f8a97eef03e291536d4e52f5147e0d
ckormanyos <ckormanyos@yahoo.com>
2025-08-07 21:48:49 +0200

    Perform more syntax improvements

--- ref_app/src/mcal/am6254_soc/mcal_port.cpp
+++ ref_app/src/mcal/am6254_soc/mcal_port.cpp
@@ -9,11 +9,16 @@
 
 void mcal::port::init(const config_type*)
 {
-  // The pad configuration of gpio0.37 is on pad37 (empirical observation).
-  // So we must initialize pad37 since we will use gpio0.36 as the
-  // benchmark toggle-pin.
+  // Initialize the benchmark toggle pin explicitly, even though
+  // this is also done in the application benchmark when one
+  // of the benchmarks is active.
+
   mcal::port::port_pin<mcal::reg::gpio0, 36U>::set_pin_low();
   mcal::port::port_pin<mcal::reg::gpio0, 36U>::set_direction_output();
+
+  // Empirical observation seemingly reveals that the pad configuration
+  // of gpio0.36 is mistakenly on pad37. So we must initialize pad37
+  // as well since we will use gpio0.36 as the benchmark toggle-pin.
 
   mcal::port::port_pin<mcal::reg::gpio0, 37U>::set_pin_low();
   mcal::port::port_pin<mcal::reg::gpio0, 37U>::set_direction_output();
