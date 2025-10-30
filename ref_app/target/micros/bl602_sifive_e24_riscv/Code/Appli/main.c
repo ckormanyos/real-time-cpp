e80038c55ed9c89f96d4381934c3b4bcdbdac671
ckormanyos <ckormanyos@yahoo.com>
2025-10-30 15:16:30 +0100

    Remove unused files BL602

--- ref_app/target/micros/bl602_sifive_e24_riscv/Code/Appli/main.c
+++ ref_app/target/micros/bl602_sifive_e24_riscv/Code/Appli/main.c
@@ -35,10 +35,12 @@
 //-----------------------------------------------------------------------------------------
 // Includes
 //-----------------------------------------------------------------------------------------
-#include <stdint.h> 
+
 #include "BL602.h"
+
 #include "riscv-csr.h"
-#include "printf.h"
+
+#include <stdint.h>
 
 //-----------------------------------------------------------------------------------------
 // Defines
@@ -85,29 +87,6 @@ int main(void)
 
   /* set the timeout to 500ms */
   CLIC_MTIMECMP = (uint64_t)(CLIC_MTIME + TIMEOUT_500MS);
-
-  /* configure gpio function for uart tx and rx */
-  glb->GPIO_CFGCTL8.bit.reg_gpio_16_func_sel = GPIO_FUN_UART;
-  glb->GPIO_CFGCTL3.bit.reg_gpio_7_func_sel  = GPIO_FUN_UART;
-
-  /* select uart function for each gpio pin */
-  glb->UART_SIG_SEL_0.bit.uart_sig_0_sel=2u; /* gpio_16(SIG0) --> UART0_TXD (2) */
-  glb->UART_SIG_SEL_0.bit.uart_sig_7_sel=3u; /* gpio_7(SIG7)  --> UART0_RXD (3) */
-
-  /* set the uart clock freq to 160MHz */
-  HBN->HBN_GLB.bit.hbn_uart_clk_sel = 1u;
-
-  /* set uart baudrate to 115200 kbps */
-  uart->uart_bit_prd.bit.cr_utx_bit_prd = 1389;
-  uart->uart_bit_prd.bit.cr_urx_bit_prd = 1389;
-
-  /* enable uart tx */
-  uart->utx_config.bit.cr_utx_frm_en = 1u;
-  uart->utx_config.bit.cr_utx_len = 1u;
-  uart->utx_config.bit.cr_utx_en = 1u;
-
-  /* output a text message on the uart console */
-  printf("BareMetal BL602 SW is Alive!\n\r");
 
   /* endless loop */
   while(1);
