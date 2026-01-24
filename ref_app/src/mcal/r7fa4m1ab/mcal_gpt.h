6b10635f07efb5d12288406d0a2bb1d3e120d78d
ckormanyos <ckormanyos@yahoo.com>
2026-01-24 17:32:50 +0100

    Initial support r7fa4m1ab via Chalandi arch

---
+++ ref_app/src/mcal/r7fa4m1ab/mcal_gpt.h
@@ -0,0 +1,29 @@
+﻿///////////////////////////////////////////////////////////////////////////////
+//  Copyright Christopher Kormanyos 2007 - 2025.
+//  Distributed under the Boost Software License,
+//  Version 1.0. (See accompanying file LICENSE_1_0.txt
+//  or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef MCAL_GPT_2011_10_20_H
+  #define MCAL_GPT_2011_10_20_H
+
+  #include <cstdint>
+
+  namespace mcal
+  {
+    namespace gpt
+    {
+      using config_type = void;
+      using value_type = std::uint64_t;
+
+      auto init(const config_type*) -> void;
+
+      struct secure final
+      {
+        static auto get_time_elapsed() -> value_type;
+      };
+    }
+  }
+
+#endif // MCAL_GPT_2011_10_20_H
