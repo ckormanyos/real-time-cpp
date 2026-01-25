8e9634fdb24006f2a00e4301fe0e4bd2a9d4ba64
ckormanyos <ckormanyos@yahoo.com>
2026-01-25 19:29:45 +0100

    Migrate system regs to standard form

--- ref_app/src/mcal/r7fa4m1ab/mcal_osc.cpp
+++ ref_app/src/mcal/r7fa4m1ab/mcal_osc.cpp
@@ -5,34 +5,54 @@
 //  or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
+#include <mcal_cpu.h>
 #include <mcal_osc.h>
+#include <mcal_reg.h>
 
-#include <R7FA4M1AB.h>
+#include <cstddef>
 
 auto mcal::osc::init(const config_type*) -> void
 {
-  constexpr uint32_t HOCOCR2 { UINT32_C(0x4001E037) };
+  // Disable register write protection for clock module.
+  // SYSTEM->PRCR.reg = (uint16_t)((0xA5 << 8) | 1);
+  mcal::reg::reg_access_static<std::uint32_t, std::uint16_t, mcal::reg::system_prcr, std::uint16_t { ((0xA5UL << 8U) | 1UL) }>::reg_set();
 
-  /* disable register write protection for clock module */
-  SYSTEM->PRCR.reg = (uint16_t)((0xA5 << 8) | 1);
+  // Set system clock division to 1.
+  // SYSTEM->SCKDIVCR.reg = 0;
+  mcal::reg::reg_access_static<std::uint32_t, std::uint32_t, mcal::reg::system_sckdivcr, UINT32_C(0)>::reg_set();
 
-  /*set system clock division to 1*/
-  SYSTEM->SCKDIVCR.reg = 0;
+  // set the HOCO clock frequency to 48 MHz.
+  // constexpr uint32_t HOCOCR2 { UINT32_C(0x4001E037) };
+  // *((volatile uint8_t*)HOCOCR2) = (4 << 3);
+  mcal::reg::reg_access_static<std::uint32_t, std::uint8_t, mcal::reg::unknown_hococr2, static_cast<std::uint8_t>(4UL << 3U)>::reg_set();
 
-  /* set the HOCO clock frequency to 48 MHz */
-  *((volatile uint8_t*)HOCOCR2) = (4 << 3);
+  // Switch sysclk to HOCO clock (48 MHz).
+  // SYSTEM->OPCCR.bit.OPCM = 0;
+  // while(SYSTEM->OPCCR.bit.OPCMTSF);
 
-  /* switch sysclk to HOCO clock (48 MHz) */
-  SYSTEM->OPCCR.bit.OPCM = 0;
-  while(SYSTEM->OPCCR.bit.OPCMTSF);
+  mcal::reg::reg_access_static<std::uint32_t, std::uint8_t, mcal::reg::system_opccr, UINT8_C(0)>::template reg_msk<UINT8_C(3)>();
 
-  /* switch power operation mode to high-speed mode */
-  SYSTEM->HOCOCR.bit.HCSTP = 0;
-  while(!SYSTEM->OSCSF.bit.HOCOSF);
+  while(mcal::reg::reg_access_static<std::uint32_t, std::uint8_t, mcal::reg::system_opccr, UINT8_C(4)>::bit_get())
+  {
+    mcal::cpu::nop();
+  }
 
-  /* enable memory wait cycle */
-  SYSTEM->MEMWAIT.bit.MEMWAIT = 1;
+  // Switch power operation mode to high-speed mode.
+  // SYSTEM->HOCOCR.bit.HCSTP = 0;
+  // while(!SYSTEM->OSCSF.bit.HOCOSF);
 
-  /* switch sysclk to HOCO clock (48 MHz) */
-  SYSTEM->SCKSCR.reg = 0;
+  mcal::reg::reg_access_static<std::uint32_t, std::uint8_t, mcal::reg::system_hococr, UINT8_C(0)>::bit_clr();
+
+  while(!mcal::reg::reg_access_static<std::uint32_t, std::uint8_t, mcal::reg::system_oscsf, UINT8_C(0)>::bit_get())
+  {
+    mcal::cpu::nop();
+  }
+
+  // Enable memory wait cycle.
+  // SYSTEM->MEMWAIT.bit.MEMWAIT = 1;
+  mcal::reg::reg_access_static<std::uint32_t, std::uint8_t, mcal::reg::system_memwait, UINT8_C(0)>::bit_set();
+
+  // Switch sysclk to HOCO clock (48 MHz).
+  // SYSTEM->SCKSCR.reg = 0;
+  mcal::reg::reg_access_static<std::uint32_t, std::uint8_t, mcal::reg::system_sckscr, UINT8_C(0)>::reg_set();
 }
--- ref_app/src/mcal/r7fa4m1ab/mcal_reg.h
+++ ref_app/src/mcal/r7fa4m1ab/mcal_reg.h
@@ -14,8 +14,19 @@
   {
     namespace reg
     {
-    }
-  }
+      // System registers.
+      constexpr std::uint32_t system_base       { UINT32_C(0x4001E000) };
+      constexpr std::uint32_t unknown_hococr2   { UINT32_C(0x4001E037) };
+
+      constexpr std::uint32_t system_sckdivcr   { system_base + UINT32_C(0x00000020) };
+      constexpr std::uint32_t system_sckscr     { system_base + UINT32_C(0x00000026) };
+      constexpr std::uint32_t system_memwait    { system_base + UINT32_C(0x00000031) };
+      constexpr std::uint32_t system_hococr     { system_base + UINT32_C(0x00000036) };
+      constexpr std::uint32_t system_oscsf      { system_base + UINT32_C(0x0000003C) };
+      constexpr std::uint32_t system_opccr      { system_base + UINT32_C(0x000000A0) };
+      constexpr std::uint32_t system_prcr       { system_base + UINT32_C(0x000003FE) };
+    } // namespace reg
+  } // namespace mcal
 
   #include <mcal/mcal_reg_access_dynamic.h>
   #include <mcal/mcal_reg_access_static.h>
